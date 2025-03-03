79f0f5debaa45c441579b8420cb99ce380ec8494
CanbiZ <47820557+MickLesk@users.noreply.github.com>
2025-03-03 12:03:17 +0100

    fix path

--- frontend/src/app/api/categories/route.ts
+++ frontend/src/app/api/categories/route.ts
@@ -5,7 +5,7 @@ import path from "path";
 
 export const dynamic = "force-static";
 
-const jsonDir = "public/json";
+const jsonDir = path.join(__dirname, '../../json');
 const metadataFileName = "metadata.json";
 const encoding = "utf-8";
 
