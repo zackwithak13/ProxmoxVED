ffed8f67de60bfadada89d6eabd78032dea5c8c0
CanbiZ <47820557+MickLesk@users.noreply.github.com>
2025-03-06 12:14:00 +0100

    fix update path check

--- ct/inventree.sh
+++ ct/inventree.sh
@@ -24,7 +24,7 @@ function update_script() {
     check_container_storage
     check_container_resources
 
-    if [[ ! -d "/opt/habitica" ]]; then
+    if [[ ! -d "/opt/inventree" ]]; then
         msg_error "No ${APP} Installation Found!"
         exit
     fi
