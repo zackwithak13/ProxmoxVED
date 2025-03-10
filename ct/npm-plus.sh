21918f69397d0a2181c43f3e56b69bac8c75e059
CanbiZ <47820557+MickLesk@users.noreply.github.com>
2025-03-10 15:10:27 +0100

    add npm plus alpine

---
+++ ct/npm-plus.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+source <(curl -s https://raw.githubusercontent.com/community-scripts/ProxmoxVED/main/misc/build.func)
+# Copyright (c) 2021-2025 tteck
+# Author: tteck (tteckster)
+# License: MIT | https://github.com/community-scripts/ProxmoxVED/raw/main/LICENSE
+# Source: https://alpinelinux.org/
+
+APP="Alpine"
+var_tags="os;alpine"
+var_cpu="1"
+var_ram="512"
+var_disk="0.1"
+var_os="alpine"
+var_version="3.21"
+var_unprivileged="1"
+
+header_info "$APP"
+variables
+color
+catch_errors
+
+function update_script() {
+  UPD=$(whiptail --backtitle "Proxmox VE Helper Scripts" --title "SUPPORT" --radiolist --cancel-button Exit-Script "Spacebar = Select" 11 58 1 \
+    "1" "Check for Alpine Updates" ON \
+    3>&1 1>&2 2>&3)
+
+  header_info
+  if [ "$UPD" == "1" ]; then
+    apk update && apk upgrade
+    exit
+  fi
+}
+
+start
+build_container
+description
+
+msg_ok "Completed Successfully!\n"
