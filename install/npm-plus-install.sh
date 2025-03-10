815b915751feb1c7d49a7fd8ceb8e4b35636f30b
CanbiZ <47820557+MickLesk@users.noreply.github.com>
2025-03-10 15:31:36 +0100

    add docker installation

--- install/npm-plus-install.sh
+++ install/npm-plus-install.sh
@@ -31,46 +31,29 @@ msg_ok "Installed Docker"
 get_latest_release() {
     curl -sL https://api.github.com/repos/$1/releases/latest | grep '"tag_name":' | cut -d'"' -f4
 }
-PORTAINER_LATEST_VERSION=$(get_latest_release "portainer/portainer")
 DOCKER_COMPOSE_LATEST_VERSION=$(get_latest_release "docker/compose")
-PORTAINER_AGENT_LATEST_VERSION=$(get_latest_release "portainer/agent")
 
-read -r -p "Would you like to add Portainer? <y/N> " prompt
-if [[ "${prompt,,}" =~ ^(y|yes)$ ]]; then
-    msg_info "Installing Portainer $PORTAINER_LATEST_VERSION"
-    docker volume create portainer_data >/dev/null
-    $STD docker run -d \
-        -p 8000:8000 \
-        -p 9443:9443 \
-        --name=portainer \
-        --restart=always \
-        -v /var/run/docker.sock:/var/run/docker.sock \
-        -v portainer_data:/data \
-        portainer/portainer-ce:latest
-    msg_ok "Installed Portainer $PORTAINER_LATEST_VERSION"
-else
-    read -r -p "Would you like to add the Portainer Agent? <y/N> " prompt
-    if [[ "${prompt,,}" =~ ^(y|yes)$ ]]; then
-        msg_info "Installing Portainer agent $PORTAINER_AGENT_LATEST_VERSION"
-        $STD docker run -d \
-            -p 9001:9001 \
-            --name portainer_agent \
-            --restart=always \
-            -v /var/run/docker.sock:/var/run/docker.sock \
-            -v /var/lib/docker/volumes:/var/lib/docker/volumes \
-            portainer/agent
-        msg_ok "Installed Portainer Agent $PORTAINER_AGENT_LATEST_VERSION"
-    fi
-fi
-read -r -p "Would you like to add Docker Compose? <y/N> " prompt
-if [[ "${prompt,,}" =~ ^(y|yes)$ ]]; then
-    msg_info "Installing Docker Compose $DOCKER_COMPOSE_LATEST_VERSION"
-    DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
-    mkdir -p $DOCKER_CONFIG/cli-plugins
-    curl -sSL https://github.com/docker/compose/releases/download/$DOCKER_COMPOSE_LATEST_VERSION/docker-compose-linux-x86_64 -o ~/.docker/cli-plugins/docker-compose
-    chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
-    msg_ok "Installed Docker Compose $DOCKER_COMPOSE_LATEST_VERSION"
-fi
+msg_info "Installing Docker Compose $DOCKER_COMPOSE_LATEST_VERSION"
+DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
+mkdir -p $DOCKER_CONFIG/cli-plugins
+curl -sSL https://github.com/docker/compose/releases/download/$DOCKER_COMPOSE_LATEST_VERSION/docker-compose-linux-x86_64 -o ~/.docker/cli-plugins/docker-compose
+chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
+msg_ok "Installed Docker Compose $DOCKER_COMPOSE_LATEST_VERSION"
+
+msg_info "Get NginxProxyManager Plus"
+cd /opt
+wget -q https://raw.githubusercontent.com/ZoeyVid/NPMplus/refs/heads/develop/compose.yaml
+msg_ok "Get NginxProxyManager Plus"
+
+read -r -p "Enter your TZ Timezone (https://en.wikipedia.org/wiki/List_of_tz_database_time_zones#List): " TZ_INPUT
+read -r -p "Enter your ACME Email: " ACME_EMAIL_INPUT
+
+sed -i "s|TZ=.*|TZ=$TZ_INPUT|g" /opt/compose.yaml
+sed -i "s|ACME_EMAIL=.*|ACME_EMAIL=$ACME_EMAIL_INPUT|g" /opt/compose.yaml
+
+msg_info "Starting NPM Plus"
+docker compose up -d
+msg_ok "Started NPM Plus"
 
 motd_ssh
 customize
