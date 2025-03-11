8f0a8105117bc7880c7aebbca0aef1bd867e1e85
CanbiZ <47820557+MickLesk@users.noreply.github.com>
2025-03-11 16:53:55 +0100

    Update validate-json.test.ts

--- frontend/src/__tests__/public/validate-json.test.ts
+++ frontend/src/__tests__/public/validate-json.test.ts
@@ -8,8 +8,11 @@ const jsonDir = "public/json";
 const metadataFileName = "metadata.json";
 const encoding = "utf-8";
 
-const fileNames = (await fs.readdir(jsonDir))
-  .filter((fileName) => fileName !== metadataFileName)
+const fileNames = (await fs.readdir(jsonDir)).filter(async (fileName) => {
+  const filePath = path.resolve(jsonDir, fileName);
+  const stat = await fs.stat(filePath);
+  return stat.isFile();
+});
 
 describe.each(fileNames)("%s", async (fileName) => {
   let script: Script;
