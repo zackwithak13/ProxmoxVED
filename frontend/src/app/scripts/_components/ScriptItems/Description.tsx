889b3235746655b19eef005bf0f713cf1798ee4f
Michel Roegl-Brunner <michel.roegl-brunner@htl-braunau.at>
2025-03-12 14:09:01 +0100

    frontend

--- frontend/src/app/scripts/_components/ScriptItems/Description.tsx
+++ frontend/src/app/scripts/_components/ScriptItems/Description.tsx
@@ -1,11 +1,21 @@
 import TextCopyBlock from "@/components/TextCopyBlock";
 import { Script } from "@/lib/types";
+import { AlertColors } from "@/config/siteConfig";
+import { AlertCircle, NotepadText } from "lucide-react";
+import { cn } from "@/lib/utils";
 
 export default function Description({ item }: { item: Script }) {
   return (
     <div className="p-2">
       <h2 className="mb-2 max-w-prose text-lg font-semibold">Description</h2>
-      <p className="text-sm text-muted-foreground">
+      <p className={cn(
+                "inline-flex items-center gap-2 rounded-lg border p-2 pl-4 text-lg pr-4",
+                AlertColors["warning"],
+              )} >
+        <AlertCircle className="h-4 min-h-4 w-4 min-w-4" />
+              <span>Unly use for testing, not in production!</span>
+      </p>
+      <p className="text-sm text-muted-foreground pt-4">
         {TextCopyBlock(item.description)}
       </p>
     </div>
