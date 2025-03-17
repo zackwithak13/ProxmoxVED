5fb46f7182d3389ea2a3b94c1541bf6b663f0907
CanbiZ <47820557+MickLesk@users.noreply.github.com>
2025-03-17 09:38:22 +0100

    Update install/wazuh-install.sh
    Co-authored-by: Bas van den Berg <74251551+bvdberg01@users.noreply.github.com>

--- install/wazuh-install.sh
+++ install/wazuh-install.sh
@@ -22,7 +22,7 @@ msg_ok "Installed Dependencies"
 
 # Fetching the latest Wazuh version
 msg_info "Fetching Latest Wazuh Version"
-RELEASE=$(curl -s https://api.github.com/repos/wazuh/wazuh/releases/latest | grep '"tag_name"' | awk -F '"' '{print substr($4, 2)}')
+RELEASE=$(curl -s https://api.github.com/repos/wazuh/wazuh/releases/latest | grep '"tag_name"' | awk -F '"' '{print substr($4, 2, length($2)-4)}')
 msg_ok "Latest Wazuh Version: $RELEASE"
 
 msg_info "Setup Wazuh"
