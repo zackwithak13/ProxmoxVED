39aa685de042cc6218f0fadb7a096e892e5321ed
CanbiZ <47820557+MickLesk@users.noreply.github.com>
2025-03-18 09:14:21 +0100

    Update docspell-install.sh

--- install/docspell-install.sh
+++ install/docspell-install.sh
@@ -74,12 +74,12 @@ mv dsc_amd* dsc
 chmod +x dsc
 mv dsc /usr/bin
 ln -s /etc/docspell-joex /opt/docspell/docspell-joex && ln -s /etc/docspell-restserver /opt/docspell/docspell-restserver && ln -s /usr/bin/dsc /opt/docspell/dsc
-sed -i -E "
-  s|bind[[:space:]]*\\{[[:space:]]*address[[:space:]]*=.*|bind { address = \"0.0.0.0\"|;
-  s|url = \"jdbc:postgresql://server:5432/db\"|url = \"jdbc:postgresql://localhost:5432/$DB_NAME\"|;
-  s|user = \".*\"|user = \"$DB_USER\"|;
-  s|password = \".*\"|password = \"$DB_PASS\"|;
-" /opt/docspell/docspell-restserver/docspell-server.conf /opt/docspell/docspell-joex.conf
+#sed -i -E "
+#  s|bind[[:space:]]*\\{[[:space:]]*address[[:space:]]*=.*|bind { address = \"0.0.0.0\"|;
+#  s|url = \"jdbc:postgresql://server:5432/db\"|url = \"jdbc:postgresql://localhost:5432/$DB_NAME\"|;
+#  s|user = \".*\"|user = \"$DB_USER\"|;
+#  s|password = \".*\"|password = \"$DB_PASS\"|;
+#" /opt/docspell/docspell-restserver/docspell-server.conf /opt/docspell/docspell-joex.conf
 msg_ok "Setup Docspell"
 
 msg_info "Setup Apache Solr"
