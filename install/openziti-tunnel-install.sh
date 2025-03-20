ed5553388c5f80c0292f025550a240db57f6ac93
Edward Moscardini <edward.moscardini@netfoundry.io>
2025-03-20 16:58:52 -0400

    switch authors

--- install/openziti-tunnel-install.sh
+++ install/openziti-tunnel-install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Copyright (c) 2021-2025 community-scripts ORG
-# Author: openziti
+# Author: emoscardini
 # License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
 # Source: https://github.com/openziti/ziti
 
