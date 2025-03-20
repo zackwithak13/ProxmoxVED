696424270237abd99c3173185413be85a7d7af85
Edward Moscardini <57814088+emoscardini@users.noreply.github.com>
2025-03-20 16:45:54 -0400

    Update install/openziti-tunnel-install.sh
    Co-authored-by: Slaviša Arežina <58952836+tremor021@users.noreply.github.com>

--- install/openziti-tunnel-install.sh
+++ install/openziti-tunnel-install.sh
@@ -3,7 +3,7 @@
 # Copyright (c) 2021-2025 community-scripts ORG
 # Author: openziti
 # License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
-# Source: https://www.openziti.io
+# Source: https://github.com/openziti/ziti
 
 source /dev/stdin <<< "$FUNCTIONS_FILE_PATH"
 color
