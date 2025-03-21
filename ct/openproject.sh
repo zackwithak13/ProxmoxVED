25336d39dd1c296430dd4740120a4fddb21c6f54
Michel Roegl-Brunner <michel.roegl-brunner@htl-braunau.at>
2025-03-21 15:25:42 +0100

    Openproject

--- ct/openproject.sh
+++ ct/openproject.sh
@@ -37,3 +37,5 @@ description
 
 msg_ok "Completed Successfully!\n"
 echo -e "${CREATING}${GN}${APP} setup has been successfully initialized!${CL}"
+echo -e "${INFO}${YW} Access it using the following URL:${CL}"
+echo -e "${TAB}${GATEWAY}${BGN}meilisearch: http://${IP}/openproject${CL}"
