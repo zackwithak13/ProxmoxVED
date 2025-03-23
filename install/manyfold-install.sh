215a64b90cb578cc07d4ee86b2c81f892f7ac21e
Bas van den Berg <74251551+bvdberg01@users.noreply.github.com>
2025-03-23 18:42:23 +0100

    Update manyfold-install.sh

--- install/manyfold-install.sh
+++ install/manyfold-install.sh
@@ -53,12 +53,16 @@ $STD npm install -g npm@latest
 $STD npm install -g yarn
 msg_ok "Installed Node.js/Yarn"
 
-msg_info "Installing Ruby Version Manager"
-$STD gpg2 --keyserver keyserver.ubuntu.com --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
-curl -sSL https://get.rvm.io -o install_rvm.sh
-chmod +x install_rvm.sh
-bash install_rvm.sh stable
-msg_ok "Installed Ruby Version Manager"
+msg_info "Add ruby-build"
+mkdir -p ~/.rbenv/plugins
+git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
+msg_ok "Added ruby-build"
+# msg_info "Installing Ruby Version Manager"
+# $STD gpg2 --keyserver keyserver.ubuntu.com --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+# curl -sSL https://get.rvm.io -o install_rvm.sh
+# chmod +x install_rvm.sh
+# bash install_rvm.sh stable
+# msg_ok "Installed Ruby Version Manager"
 
 msg_info "Adding manyfold user"
 useradd -m -s /usr/bin/bash manyfold
@@ -76,8 +80,10 @@ chown -R manyfold:manyfold /opt/manyfold
 RUBY_VERSION=$(cat .ruby-version)
 YARN_VERSION=$(grep '"packageManager":' package.json | sed -E 's/.*"(yarn@[0-9\.]+)".*/\1/')
 $STD gem install bundler
-/bin/bash --login -c "rvm install $RUBY_VERSION"
-sudo -u manyfold bash -c "source /etc/profile.d/rvm.sh && rvm use $RUBY_VERSION && bundle install"
+# /bin/bash --login -c "rvm install $RUBY_VERSION"
+rbenv install $RUBY_VERSION
+rbenv global $RUBY_VERSION
+sudo -u manyfold bash -c "bundle install"
 $STD gem install sidekiq
 $STD npm install --global corepack
 corepack enable
