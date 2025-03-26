55ae854883db7f5233eed098db260ac949b2f766
CanbiZ <47820557+MickLesk@users.noreply.github.com>
2025-03-26 14:29:57 +0100

    testing

--- ct/alpine-gitea.sh
+++ ct/alpine-gitea.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-source <(curl -s https://raw.githubusercontent.com/community-scripts/ProxmoxVED/main/misc/build.func)
+source <(curl -fsSL https://raw.githubusercontent.com/community-scripts/ProxmoxVED/main/misc/build.func)
 # Copyright (c) 2021-2025 community-scripts ORG
 # Author: MickLesk (CanbiZ)
 # License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
@@ -33,7 +33,6 @@ function update_script() {
     msg_info "Restarting Gitea"
     rc-service gitea restart
     msg_ok "Restarted Gitea"
-    exit_script
 }
 
 start
