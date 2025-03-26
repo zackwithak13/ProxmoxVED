f942d17845a060ffaeaa907943e27fd4ca34b5cd
Edward Moscardini <edward.moscardini@netfoundry.io>
2025-03-26 16:50:25 -0400

    update end of scritp echo

--- ct/openziti-controller.sh
+++ ct/openziti-controller.sh
@@ -40,4 +40,5 @@ description
 
 msg_ok "Completed Successfully!\n"
 echo -e "${CREATING}${GN}${APP} setup has been successfully initialized!${CL}"
-echo -e "Access the web interface at https://${IP}:<port>/zac"
+echo -e "${INFO}${YW} Access it using the following URL:${CL}"
+echo -e "${TAB}${GATEWAY}${BGN}http://${IP}:<port>/zac${CL}"
