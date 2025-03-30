c0bf8b9f3a7bd21628ebdd3d93222b0d0c0b9f8e
bvdberg01 <74251551+bvdberg01@users.noreply.github.com>
2025-03-30 19:18:47 +0200

    Fix path

--- ct/bar-assistant.sh
+++ ct/bar-assistant.sh
@@ -25,7 +25,7 @@ function update_script() {
     header_info
     check_container_storage
     check_container_resources
-    if [[ ! -d /opt/manyfold ]]; then
+    if [[ ! -d /opt/bar-assistant ]]; then
         msg_error "No ${APP} Installation Found!"
         exit
     fi
