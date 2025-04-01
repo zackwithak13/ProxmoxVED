37194a57245e2fba56c0c056938954d3f8544adb
CanbiZ <47820557+MickLesk@users.noreply.github.com>
2025-04-01 13:34:15 +0200

    Update ubuntu-install.sh

--- install/ubuntu-install.sh
+++ install/ubuntu-install.sh
@@ -16,7 +16,7 @@ update_os
 msg_info "Installing Dependencies"
 $STD apt-get install -y curl
 $STD apt-get install -y sudo
-$STD apt-get install -y mc
+$STD apt-get install -y jq
 msg_ok "Installed Dependencies"
 
 msg_info "Check GH Releases"
