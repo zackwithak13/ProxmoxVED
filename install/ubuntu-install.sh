f84dd07c70f186fc7f208535f46c00c3798a3e42
CanbiZ <47820557+MickLesk@users.noreply.github.com>
2025-04-01 15:52:04 +0200

    Update ubuntu-install.sh

--- install/ubuntu-install.sh
+++ install/ubuntu-install.sh
@@ -21,103 +21,103 @@ msg_ok "Installed Dependencies"
 
 msg_info "Check GH Releases"
 echo "Getting 0xERR0R/blocky..."
-RELEASE=$(get_gh_release 0xERR0R/blocky) || exit 1
+RELEASE=$(get_gh_release 0xERR0R/blocky)
 echo "Got Version: $RELEASE"
 
 echo "Getting aceberg/WatchYourLAN..."
-RELEASE=$(get_gh_release aceberg/WatchYourLAN) || exit 1
+RELEASE=$(get_gh_release aceberg/WatchYourLAN)
 echo "Got Version: $RELEASE"
 
-echo "Getting actualbudget/actual-server..."
-RELEASE=$(get_gh_release actualbudget/actual-server) || exit 1
+echo "Getting actualbudget/actual..."
+RELEASE=$(get_gh_release actualbudget/actual)
 echo "Got Version: $RELEASE"
 
 echo "Getting agl/jbig2enc..."
-RELEASE=$(get_gh_release agl/jbig2enc) || exit 1
+RELEASE=$(get_gh_release agl/jbig2enc)
 echo "Got Version: $RELEASE"
 
 echo "Getting alexta69/metube..."
-RELEASE=$(get_gh_release alexta69/metube) || exit 1
+RELEASE=$(get_gh_release alexta69/metube)
 echo "Got Version: $RELEASE"
 
 echo "Getting AlexxIT/go2rtc..."
-RELEASE=$(get_gh_release AlexxIT/go2rtc) || exit 1
+RELEASE=$(get_gh_release AlexxIT/go2rtc)
 echo "Got Version: $RELEASE"
 
 echo "Getting apache/tika..."
-RELEASE=$(get_gh_release apache/tika) || exit 1
+RELEASE=$(get_gh_release apache/tika)
 echo "Got Version: $RELEASE"
 
 echo "Getting ArtifexSoftware/ghostpdl-downloads..."
-RELEASE=$(get_gh_release ArtifexSoftware/ghostpdl-downloads) || exit 1
+RELEASE=$(get_gh_release ArtifexSoftware/ghostpdl-downloads)
 echo "Got Version: $RELEASE"
 
 echo "Getting Athou/commafeed..."
-RELEASE=$(get_gh_release Athou/commafeed) || exit 1
+RELEASE=$(get_gh_release Athou/commafeed)
 echo "Got Version: $RELEASE"
 
 echo "Getting authelia/authelia..."
-RELEASE=$(get_gh_release authelia/authelia) || exit 1
+RELEASE=$(get_gh_release authelia/authelia)
 echo "Got Version: $RELEASE"
 
 echo "Getting azukaar/Cosmos-Server..."
-RELEASE=$(get_gh_release azukaar/Cosmos-Server) || exit 1
+RELEASE=$(get_gh_release azukaar/Cosmos-Server)
 echo "Got Version: $RELEASE"
 
 echo "Getting bastienwirtz/homer..."
-RELEASE=$(get_gh_release bastienwirtz/homer) || exit 1
+RELEASE=$(get_gh_release bastienwirtz/homer)
 echo "Got Version: $RELEASE"
 
 echo "Getting benjaminjonard/koillection..."
-RELEASE=$(get_gh_release benjaminjonard/koillection) || exit 1
+RELEASE=$(get_gh_release benjaminjonard/koillection)
 echo "Got Version: $RELEASE"
 
 echo "Getting benzino77/tasmocompiler..."
-RELEASE=$(get_gh_release benzino77/tasmocompiler) || exit 1
+RELEASE=$(get_gh_release benzino77/tasmocompiler)
 echo "Got Version: $RELEASE"
 
 echo "Getting blakeblackshear/frigate..."
-RELEASE=$(get_gh_release blakeblackshear/frigate) || exit 1
+RELEASE=$(get_gh_release blakeblackshear/frigate)
 echo "Got Version: $RELEASE"
 
 echo "Getting bluenviron/mediamtx..."
-RELEASE=$(get_gh_release bluenviron/mediamtx) || exit 1
+RELEASE=$(get_gh_release bluenviron/mediamtx)
 echo "Got Version: $RELEASE"
 
 echo "Getting BookStackApp/BookStack..."
-RELEASE=$(get_gh_release BookStackApp/BookStack) || exit 1
+RELEASE=$(get_gh_release BookStackApp/BookStack)
 echo "Got Version: $RELEASE"
 
 echo "Getting browserless/chrome..."
-RELEASE=$(get_gh_release browserless/chrome) || exit 1
+RELEASE=$(get_gh_release browserless/chrome)
 echo "Got Version: $RELEASE"
 
 echo "Getting Bubka/2FAuth..."
-RELEASE=$(get_gh_release Bubka/2FAuth) || exit 1
+RELEASE=$(get_gh_release Bubka/2FAuth)
 echo "Got Version: $RELEASE"
 
 echo "Getting caddyserver/xcaddy..."
-RELEASE=$(get_gh_release caddyserver/xcaddy) || exit 1
+RELEASE=$(get_gh_release caddyserver/xcaddy)
 echo "Got Version: $RELEASE"
 
 echo "Getting clusterzx/paperless-ai..."
-RELEASE=$(get_gh_release clusterzx/paperless-ai) || exit 1
+RELEASE=$(get_gh_release clusterzx/paperless-ai)
 echo "Got Version: $RELEASE"
 
 echo "Getting cockpit-project/cockpit..."
-RELEASE=$(get_gh_release cockpit-project/cockpit) || exit 1
+RELEASE=$(get_gh_release cockpit-project/cockpit)
 echo "Got Version: $RELEASE"
 
 echo "Getting community-scripts/ProxmoxVE..."
-RELEASE=$(get_gh_release community-scripts/ProxmoxVE) || exit 1
+RELEASE=$(get_gh_release community-scripts/ProxmoxVE)
 echo "Got Version: $RELEASE"
 
 echo "Getting CorentinTh/it-tools..."
-RELEASE=$(get_gh_release CorentinTh/it-tools) || exit 1
+RELEASE=$(get_gh_release CorentinTh/it-tools)
 echo "Got Version: $RELEASE"
 
 echo "Getting dani-garcia/bw_web_builds..."
-RELEASE=$(get_gh_release dani-garcia/bw_web_builds) || exit 1
+RELEASE=$(get_gh_release dani-garcia/bw_web_builds)
 echo "Got Version: $RELEASE"
 
 msg_ok "Done"
