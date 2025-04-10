aa1451ff26e92fa44934ec18b528eca5e7161ed5
elvito <elvito@users.noreply.github.com>
2025-04-10 19:10:03 +0200

    Create umlautadaptarr.sh

---
+++ ct/umlautadaptarr.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+source <(curl -fsSL https://raw.githubusercontent.com/community-scripts/ProxmoxVED/main/misc/build.func)
+# Copyright (c) 2021-2025 community-scripts ORG
+# Author: elvito
+# License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
+# Source: https://github.com/PCJones/UmlautAdaptarr
+
+APP="Umlautadaptarr"
+var_tags="arr"
+var_cpu="1"
+var_ram="512"
+var_disk="4"
+var_os="debian"
+var_version="12"
+var_unprivileged="1"
+
+header_info "$APP"
+variables
+color
+catch_errors
+
+function update_script() {
+    header_info
+    check_container_storage
+    check_container_resources
+
+    # Check if installation is present | -f for file, -d for folder
+    if [[ ! -f /opt/UmlautAdaptarr/appsettings.json ]]; then
+        msg_error "No ${APP} Installation Found!"
+        exit
+    fi
+    msg_info "Updating $APP..."
+    $STD cd /opt/UmlautAdaptarr || exit
+    $STD git pull origin master
+    $STD dotnet restore
+    $STD dotnet build --configuration Release
+    $STD systemctl restart umlautadaptarr
+    msg_ok "$APP has been updated."
+    exit
+}
+
+start
+build_container
+description
+
+msg_ok "Completed Successfully!\n"
+echo -e "${CREATING}${GN}${APP} setup has been successfully initialized!${CL}"
+echo -e "${INFO}${YW} Access it using the following URL:${CL}"
+echo -e "${TAB}${GATEWAY}${BGN}http://${IP}:[PORT]${CL}"
