02ae2e010967b3f56f331adf51d99a8d0c36f508
Nícolas Pastorello <nicolaspastorello1@gmail.com>
2025-04-12 15:04:19 -0300

    Update install/jumpserver-install.sh
    Co-authored-by: Slaviša Arežina <58952836+tremor021@users.noreply.github.com>

--- install/jumpserver-install.sh
+++ install/jumpserver-install.sh
@@ -3,7 +3,7 @@
 # Copyright (c) 2021-2025 community-scripts ORG
 # Author: Nícolas Pastorello (opastorello)
 # License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
-# Source: https://jumpserver.com/
+# Source: https://github.com/jumpserver/jumpserver
 
 source /dev/stdin <<<"$FUNCTIONS_FILE_PATH"
 color
