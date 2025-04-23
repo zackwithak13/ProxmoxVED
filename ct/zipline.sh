baba828bdb9c1f93f0ea4d165a041ee0fa6cc95f
Michel Roegl-Brunner <michel.roegl-brunner@htl-braunau.at>
2025-04-23 08:16:48 +0200

    Add Zipline for testing

---
+++ ct/zipline.sh
@@ -0,0 +1,75 @@
+#!/usr/bin/env bash
+source <(curl -fsSL https://raw.githubusercontent.com/community-scripts/ProxmoxVED/main/misc/build.func)
+# Copyright (c) 2021-2025 tteck
+# Author: MickLesk (Canbiz)
+# License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
+# Source: https://zipline.diced.sh/
+
+APP="Zipline"
+var_tags="${var_tags:-file;sharing}"
+var_cpu="${var_cpu:-2}"
+var_ram="${var_ram:-2048}"
+var_disk="${var_disk:-5}"
+var_os="${var_os:-debian}"
+var_version="${var_version:-12}"
+var_unprivileged="${var_unprivileged:-1}"
+
+header_info "$APP"
+variables
+color
+catch_errors
+function update_script() {
+  header_info
+  check_container_storage
+  check_container_resources
+  if [[ ! -d /opt/zipline ]]; then
+    msg_error "No ${APP} Installation Found!"
+    exit
+  fi
+  if ! command -v pnpm &>/dev/null; then
+    msg_info "Installing pnpm"
+    #export NODE_OPTIONS=--openssl-legacy-provider
+    $STD npm install -g pnpm@latest
+    msg_ok "Installed pnpm"
+  fi
+  RELEASE=$(curl -fsSL https://api.github.com/repos/diced/zipline/releases/latest | grep "tag_name" | awk '{print substr($2, 3, length($2)-4) }')
+  if [[ ! -f /opt/${APP}_version.txt ]] || [[ "${RELEASE}" != "$(cat /opt/${APP}_version.txt)" ]]; then
+    msg_info "Stopping ${APP}"
+    systemctl stop zipline
+    msg_ok "${APP} Stopped"
+
+    msg_info "Updating ${APP} to ${RELEASE}"
+    cp /opt/zipline/.env /opt/
+    rm -R /opt/zipline
+    curl -fsSL "https://github.com/diced/zipline/archive/refs/tags/v${RELEASE}.zip" -o $(basename "https://github.com/diced/zipline/archive/refs/tags/v${RELEASE}.zip")
+    unzip -q v${RELEASE}.zip
+    mv zipline-${RELEASE} /opt/zipline
+    cd /opt/zipline
+    mv /opt/.env /opt/zipline/.env
+    $STD pnpm install
+    $STD pnpm build
+    echo "${RELEASE}" >/opt/${APP}_version.txt
+    msg_ok "Updated ${APP}"
+
+    msg_info "Starting ${APP}"
+    systemctl start zipline
+    msg_ok "Started ${APP}"
+
+    msg_info "Cleaning Up"
+    rm -rf v${RELEASE}.zip
+    msg_ok "Cleaned"
+    msg_ok "Updated Successfully"
+  else
+    msg_ok "No update required. ${APP} is already at ${RELEASE}"
+  fi
+  exit
+}
+
+start
+build_container
+description
+
+msg_ok "Completed Successfully!\n"
+echo -e "${CREATING}${GN}${APP} setup has been successfully initialized!${CL}"
+echo -e "${INFO}${YW} Access it using the following URL:${CL}"
+echo -e "${TAB}${GATEWAY}${BGN}http://${IP}:3000${CL}"
