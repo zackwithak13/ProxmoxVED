ea8983bdd60c9536d5a76a50af6da6c625e66c95
Michel Roegl-Brunner <michel.roegl-brunner@htl-braunau.at>
2025-04-24 10:02:11 +0200

    Handel Responsive/Mobile

--- frontend/src/app/scripts/_components/ScriptItems/ConfigFile.tsx
+++ frontend/src/app/scripts/_components/ScriptItems/ConfigFile.tsx
@@ -1,10 +1,10 @@
-import CodeCopyButton from "@/components/ui/code-copy-button";
+import ConfigCopyButton from "@/components/ui/config-copy-button";
 import { Script } from "@/lib/types";
 
 export default function ConfigFile({ item }: { item: Script }) {
   return (
     <div className="px-4 pb-4">
-      <CodeCopyButton>{item.config_path ? item.config_path : "No config path set"}</CodeCopyButton>
+      <ConfigCopyButton>{item.config_path ? item.config_path : "No config path set"}</ConfigCopyButton>
     </div>
   );
 }
