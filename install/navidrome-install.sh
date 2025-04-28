c5f2b84acdc83c9c07b5cc4aea143b317f95bf96
CanbiZ <47820557+MickLesk@users.noreply.github.com>
2025-04-28 15:52:19 +0200

    navidrome

---
+++ install/navidrome-install.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+# Copyright (c) 2021-2025 community-scripts ORG
+# Author: MickLesk (CanbiZ)
+# License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
+# Source: https://github.com/navidrome/navidrome
+
+source /dev/stdin <<<"$FUNCTIONS_FILE_PATH"
+color
+verb_ip6
+catch_errors
+setting_up_container
+network_check
+update_os
+
+msg_info "Installing Dependencies (Patience)"
+$STD apt-get install -y \
+    ffmpeg
+msg_ok "Installed Dependencies"
+
+msg_info "Installing Navidrome"
+RELEASE=$(curl -fsSL https://api.github.com/repos/navidrome/navidrome/releases/latest | grep "tag_name" | awk -F '"' '{print $4}')
+TMP_DEB=$(mktemp --suffix=.deb)
+curl -fsSL -o "${TMP_DEB}" "https://github.com/navidrome/navidrome/releases/download/${RELEASE}/navidrome_${RELEASE#v}_linux_amd64.deb"
+systemctl enable -q --now navidrome.service
+$STD apt-get install -y "${TMP_DEB}"
+msg_ok "Installed hoodik"
+
+motd_ssh
+customize
+
+msg_info "Cleaning up"
+rm -f "${TMP_DEB}"
+$STD apt-get autoremove
+$STD apt-get autoclean
+msg_ok "Cleaned"
