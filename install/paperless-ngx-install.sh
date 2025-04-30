624bc4e5d67692e11fccfee76c597b4ea1492afc
CanbiZ <47820557+MickLesk@users.noreply.github.com>
2025-04-30 09:18:13 +0200

    Update paperless-ngx-install.sh

--- install/paperless-ngx-install.sh
+++ install/paperless-ngx-install.sh
@@ -18,6 +18,8 @@ $STD apt-get install -y \
     redis \
     postgresql \
     python3-dev \
+    python3-setuptools \
+    python3-wheel \
     build-essential \
     imagemagick \
     fonts-liberation \
@@ -39,26 +41,26 @@ $STD apt-get install -y \
 setup_uv
 msg_ok "Installed Dependencies"
 
-# msg_info "Installing OCR Dependencies (Patience)"
-# $STD apt-get install -y \
-#     unpaper \
-#     icc-profiles-free \
-#     qpdf \
-#     liblept5 \
-#     libxml2 \
-#     pngquant \
-#     zlib1g \
-#     tesseract-ocr \
-#     tesseract-ocr-eng
+msg_info "Installing OCR Dependencies (Patience)"
+$STD apt-get install -y \
+    unpaper \
+    icc-profiles-free \
+    qpdf \
+    liblept5 \
+    libxml2 \
+    pngquant \
+    zlib1g \
+    tesseract-ocr \
+    tesseract-ocr-eng
 
-# cd /tmp
-# curl -fsSL "https://github.com/ArtifexSoftware/ghostpdl-downloads/releases/download/gs10051/ghostpdl-10.05.1.tar.gz" -o "ghostscript.tar.gz"
-# $STD tar -xzf ghostscript.tar.gz
-# cd ghostpdl-10.05.1
-# $STD ./configure
-# $STD make
-# $STD make install
-# msg_ok "Installed OCR Dependencies"
+cd /tmp
+curl -fsSL "https://github.com/ArtifexSoftware/ghostpdl-downloads/releases/download/gs10051/ghostpdl-10.05.1.tar.gz" -o "ghostscript.tar.gz"
+tar -xzf ghostscript.tar.gz
+cd ghostpdl-10.05.1
+$STD ./configure
+$STD make
+$STD make install
+msg_ok "Installed OCR Dependencies"
 
 msg_info "Installing JBIG2"
 $STD git clone https://github.com/ie13/jbig2enc /opt/jbig2enc
