7758a12d3d98c1b55035bc055381b4e241547433
CanbiZ <47820557+MickLesk@users.noreply.github.com>
2025-05-05 13:48:29 +0200

    Update streamlink-webui-install.sh

--- install/streamlink-webui-install.sh
+++ install/streamlink-webui-install.sh
@@ -24,7 +24,7 @@ fetch_and_deploy_gh_release "CrazyWolf13/streamlink-webui"
 $STD uv venv /opt/"${APPLICATION}"/backend/src/.venv
 source /opt/"${APPLICATION}"/backend/src/.venv/bin/activate
 $STD uv pip install -r /opt/streamlink-webui/backend/src/requirements.txt --python=/opt/"${APPLICATION}"/backend/src/.venv
-$STD pip install -r requirements.txt
+#$STD pip install -r requirements.txt
 cd ../../frontend/src
 $STD yarn build
 msg_ok "Setup ${APPLICATION}"
