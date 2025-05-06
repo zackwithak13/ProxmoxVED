7587b223446b3c0e5918c9d25e14c1aa6c1e93e7
CanbiZ <47820557+MickLesk@users.noreply.github.com>
2025-05-06 07:51:24 +0200

    Update fumadocs-install.sh

--- install/fumadocs-install.sh
+++ install/fumadocs-install.sh
@@ -14,19 +14,11 @@ update_os
 
 msg_info "Installing Dependencies"
 $STD apt-get install -y \
-  gnupg \
   ca-certificates \
   expect
 msg_ok "Installed Dependencies"
 
-msg_info "Installing Node.js"
-mkdir -p /etc/apt/keyrings
-curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
-echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_22.x nodistro main" >/etc/apt/sources.list.d/nodesource.list
-$STD apt-get update
-$STD apt-get install -y nodejs
-$STD npm install -g pnpm
-msg_ok "Installed Node.js"
+NODE_VERSION="22" install_node_and_modules
 
 msg_info "Installing Fumadocs"
 fetch_and_deploy_gh_release fuma-nama/fumadocs
