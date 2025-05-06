2cefdd97e8126b80e3afafb7a922adb150355228
CanbiZ <47820557+MickLesk@users.noreply.github.com>
2025-05-06 08:09:12 +0200

    Update fumadocs-install.sh

--- install/fumadocs-install.sh
+++ install/fumadocs-install.sh
@@ -22,26 +22,10 @@ NODE_VERSION="22" NODE_MODULE="pnpm@latest" install_node_and_modules
 
 msg_info "Installing Fumadocs"
 fetch_and_deploy_gh_release fuma-nama/fumadocs
-# temp_file=$(mktemp)
-# RELEASE=$(curl -fsSL https://api.github.com/repos/fuma-nama/fumadocs/releases/latest | grep '"tag_name"' | awk -F '"' '{print $4}')
 export NODE_OPTIONS="--max-old-space-size=4096"
-# curl -fsSL "https://github.com/fuma-nama/fumadocs/archive/refs/tags/${RELEASE}.tar.gz" -o "$temp_file"
-# tar -xzf $temp_file
-# mv fumadocs-* "/opt/fumadocs"
 cd /opt/fumadocs
 $STD pnpm install
 pnpm create fumadocs-app
-# expect "Project name"
-# send "my-app\r"
-# expect "Choose a template"
-# send "Next.js: Fumadocs MDX\r"
-# expect "Use \`/src\` directory?"
-# send "No\r"
-# expect "Add default ESLint configuration?"
-# send "No\r"
-# expect "Do you want to install packages automatically?*"
-# send "Yes\r"
-# expect eof
 echo "${RELEASE}" >/opt/${APPLICATION}_version.txt
 msg_ok "Installed Fumadocs"
 
@@ -58,8 +42,8 @@ Restart=always
 
 [Install]
 WantedBy=multi-user.target
-msg_ok "Created Service"
 EOF
+msg_ok "Created Service"
 
 motd_ssh
 customize
