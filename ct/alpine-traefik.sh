c0ccfbe43b07206a335d33f0fb79fc64934be662
CanbiZ <47820557+MickLesk@users.noreply.github.com>
2025-05-09 14:48:33 +0200

    Update alpine-traefik.sh

--- ct/alpine-traefik.sh
+++ ct/alpine-traefik.sh
@@ -37,3 +37,6 @@ build_container
 description
 
 msg_ok "Completed Successfully!\n"
+echo -e "${CREATING}${GN}${APP} setup has been successfully initialized!${CL}"
+echo -e "${INFO}${YW} WebUI Access (if configured) - using the following URL:${CL}"
+echo -e "${TAB}${GATEWAY}${BGN}http://${IP}:8080/dashboard${CL}"
