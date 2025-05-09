7d45266c30339b8000f734fc2a022696d367ea2a
Aliaksei Pilko <a.pilko@soton.ac.uk>
2025-05-09 16:38:24 +0100

    Change URLs back to community-scripts

--- ct/garmin-grafana.sh
+++ ct/garmin-grafana.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-source <(curl -fsSL https://raw.githubusercontent.com/aliaksei135/ProxmoxVE/refs/heads/garmin-grafana/misc/build.func)
+source <(curl -fsSL https://raw.githubusercontent.com/community-scripts/ProxmoxVED/raw/main/garmin-grafana/misc/build.func)
 # Copyright (c) 2021-2025 community-scripts ORG
 # Author: aliaksei135
 # License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
