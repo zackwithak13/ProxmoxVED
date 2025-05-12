36ca21848f64d06891aeb4347bf39342d0944ef1
CanbiZ <47820557+MickLesk@users.noreply.github.com>
2025-05-12 15:02:49 +0200

    test

--- install/ampache-install.sh
+++ install/ampache-install.sh
@@ -5,7 +5,7 @@
 # License: MIT
 # https://github.com/tteck/Proxmox/raw/main/LICENSE
 
-source /dev/stdin <<< "$FUNCTIONS_FILE_PATH"
+source /dev/stdin <<<"$FUNCTIONS_FILE_PATH"
 color
 verb_ip6
 catch_errors
@@ -15,7 +15,6 @@ update_os
 
 msg_info "Installing Dependencies (Patience)"
 apt-get install -y \
-  mariadb-server \
   apache2 \
   cron \
   flac \
@@ -27,28 +26,21 @@ apt-get install -y \
   wget \
   curl \
   git \
-  zip \
-  unzip \
-  sudo \
   make \
-  mc 
- msg_ok "Installed Dependencies"
- 
-msg_info "Setting up PHP"
-sudo curl -sSLo /usr/share/keyrings/deb.sury.org-php.gpg https://packages.sury.org/php/apt.gpg
-sudo sh -c 'echo "deb [signed-by=/usr/share/keyrings/deb.sury.org-php.gpg] https://packages.sury.org/php/ $(lsb_release -sc) main" > /etc/apt/sources.list.d/php.list'
-sudo apt update
-sudo apt install -y php8.3 php8.3-{bcmath,bz2,cli,common,curl,fpm,gd,imagick,intl,mbstring,mysql,sqlite3,xml,xmlrpc,zip}
-apt-get install -y \
-  libapache2-mod-php \
   inotify-tools \
   libavcodec-extra \
   libev-libevent-dev \
   libmp3lame-dev \
   libtheora-dev \
   libvorbis-dev \
-  libvpx-dev 
-msg_ok "PHP successfully setup"  
+  libvpx-dev
+msg_ok "Installed Dependencies"
+
+PHP_VERSION=8.4
+PHP_MODULE=bcmath,bz2,cli,common,curl,fpm,gd,imagick,intl,mbstring,mysql,sqlite3,xml,xmlrpc,zip
+PHP_APACHE=YES
+install_php
+install_mariadb
 
 msg_info "Setting up Database"
 DB_NAME=ampache2
@@ -66,8 +58,8 @@ msg_ok "Set up database"
 msg_info "Installing Ampache(Patience)"
 cd /opt
 AMPACHE_VERSION=$(wget -q https://github.com/ampache/ampache/releases/latest -O - | grep "title>Release" | cut -d " " -f 4)
-wget https://github.com/ampache/ampache/releases/download/${AMPACHE_VERSION}/ampache-${AMPACHE_VERSION}_all_php8.3.zip
-unzip -q ampache-${AMPACHE_VERSION}_all_php8.3.zip -d ampache
+wget https://github.com/ampache/ampache/releases/download/${AMPACHE_VERSION}/ampache-${AMPACHE_VERSION}_all_php8.4.zip
+unzip -q ampache-${AMPACHE_VERSION}_all_php8.4.zip -d ampache
 rm -rf /var/www/html
 ln -s /opt/ampache/public /var/www/html
 sudo mv /opt/ampache/rest/.htaccess.dist /opt/ampache/rest/.htaccess
@@ -75,11 +67,11 @@ sudo mv /opt/ampache/play/.htaccess.dist /opt/ampache/play/.htaccess
 sudo mv /opt/ampache/channel/.htaccess.dist /opt/ampache/channel/.htaccess
 sudo cp /opt/ampache/config/ampache.cfg.php.dist /opt/ampache/config/ampache.cfg.php
 sudo chmod 664 /opt/ampache/rest/.htaccess /opt/ampache/play/.htaccess
-sudo sed -i 's/upload_max_filesize = .*/upload_max_filesize = 50M/' /etc/php/8.3/apache2/php.ini \
-&& sudo sed -i 's/post_max_size = .*/post_max_size = 50M/' /etc/php/8.3/apache2/php.ini \
-&& sudo sed -i 's/max_execution_time = .*/max_execution_time = 300/' /etc/php/8.3/apache2/php.ini \
-&& sudo sed -i 's/memory_limit = .*/memory_limit = 256M/' /etc/php/8.3/apache2/php.ini \
-&& sudo systemctl restart apache2
+sudo sed -i 's/upload_max_filesize = .*/upload_max_filesize = 50M/' /etc/php/8.4/apache2/php.ini &&
+  sudo sed -i 's/post_max_size = .*/post_max_size = 50M/' /etc/php/8.4/apache2/php.ini &&
+  sudo sed -i 's/max_execution_time = .*/max_execution_time = 300/' /etc/php/8.4/apache2/php.ini &&
+  sudo sed -i 's/memory_limit = .*/memory_limit = 256M/' /etc/php/8.4/apache2/php.ini &&
+  sudo systemctl restart apache2
 msg_ok "Installed Ampache"
 
 motd_ssh
