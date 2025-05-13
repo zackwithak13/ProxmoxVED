0af4ec541fcd3b5c55f1d69aa1e2fa7c550e7699
tremor021 <arezina.slavisa@gmail.com>
2025-05-13 14:15:07 +0200

    Update Bitmagnet

--- ct/alpine-bitmagnet.sh
+++ ct/alpine-bitmagnet.sh
@@ -31,11 +31,38 @@ function update_script() {
     msg_info "Updating ${APP} LXC"
     $STD apk -U upgrade
     $STD service bitmagnet stop
+    $STD su - postgres -c "pg_dump \
+      --column-inserts \
+      --data-only \
+      --on-conflict-do-nothing \
+      --rows-per-insert=1000 \
+      --table=metadata_sources \
+      --table=content \
+      --table=content_attributes \
+      --table=content_collections \
+      --table=content_collections_content \
+      --table=torrent_sources \
+      --table=torrents \
+      --table=torrent_files \
+      --table=torrent_hints \
+      --table=torrent_contents \
+      --table=torrent_tags \
+      --table=torrents_torrent_sources \
+      --table=key_values \
+      bitmagnet \
+      >/tmp/backup.sql"
+    mv /tmp/backup.sql /opt/
+    [ -f /opt/bitmagnet/.env ] && cp /opt/bitmagnet/.env /opt/
+    [ -f /opt/bitmagnet/config.yml ] && cp /opt/bitmagnet/config.yml /opt/
+    rm -rf /opt/bitmagnet/*
     temp_file=$(mktemp)
     curl -fsSL "https://github.com/bitmagnet-io/bitmagnet/archive/refs/tags/v${RELEASE}.tar.gz" -o "$temp_file"
     tar zxf "$temp_file" --strip-components=1 -C /opt/bitmagnet
     cd /opt/bitmagnet
-    $STD go build -ldflags "-s -w -X github.com/bitmagnet-io/bitmagnet/internal/version.GitTag=$(git describe --tags --always --dirty)"
+    $STD go build
+    chmod +x bitmagnet
+    [ -f "/opt/.env" ] && cp "/opt/.env" /opt/bitmagnet/
+    [ -f "/opt/config.yml" ] && cp "/opt/config.yml" /opt/bitmagnet/
     rm -f "$temp_file"
     echo "${RELEASE}" >/opt/bitmagnet_version.txt
     $STD service bitmagnet start
@@ -54,4 +81,4 @@ description
 msg_ok "Completed Successfully!\n"
 echo -e "${CREATING}${GN}${APP} setup has been successfully initialized!${CL}"
 echo -e "${INFO}${YW} Access it using the following IP:${CL}"
-echo -e "${TAB}${GATEWAY}${BGN}http://${IP}:8080${CL}"
+echo -e "${TAB}${GATEWAY}${BGN}http://${IP}:3333${CL}"
