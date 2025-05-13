5fd366e9b1afcddf24b773c074c5cac939768935
enirts31 <ksad.karim@gmail.com>
2025-05-13 09:34:22 -0400

    Update ct/backrest.sh
    Co-authored-by: Slaviša Arežina <58952836+tremor021@users.noreply.github.com>

--- ct/backrest.sh
+++ ct/backrest.sh
@@ -27,7 +27,6 @@ function update_script() {
     msg_error "No ${APP} Installation Found!"
     exit
   fi
-
   RELEASE=$(curl -fsSL https://api.github.com/repos/garethgeorge/backrest/releases/latest | grep "tag_name" | awk '{print substr($2, 3, length($2)-4) }')
   if [[ ! -f /opt/${APP}_version.txt ]] || [[ "${RELEASE}" != "$(cat /opt/${APP}_version.txt)" ]]; then
     msg_info "Stopping ${APP}"
