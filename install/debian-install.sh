1110ed29fcc4f3ddc31fad45274202c2a7c86655
CanbiZ <47820557+MickLesk@users.noreply.github.com>
2025-05-15 15:39:21 +0200

    test

--- install/debian-install.sh
+++ install/debian-install.sh
@@ -104,11 +104,11 @@ $STD apt-get -y autoremove
 $STD apt-get -y autoclean
 msg_ok "Cleaned"
 
-read -p "Remove this script? <y/N> " prompt
-if [[ "${prompt,,}" =~ ^(y|yes)$ ]]; then
-  pct stop "$CTID"
-  pct remove "$CTID"
-  msg_ok "Removed this script"
-else
-  msg_warn "Did not remove this script"
-fi
+# read -p "Remove this script? <y/N> " prompt
+# if [[ "${prompt,,}" =~ ^(y|yes)$ ]]; then
+#   pct stop "$CTID"
+#   pct remove "$CTID"
+#   msg_ok "Removed this script"
+# else
+#   msg_warn "Did not remove this script"
+# fi
