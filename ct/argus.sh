5cce8148317a4554ed5229908c811f6085cb61d3
CanbiZ <47820557+MickLesk@users.noreply.github.com>
2025-05-20 17:10:22 +0200

    Update argus.sh

--- ct/argus.sh
+++ ct/argus.sh
@@ -13,6 +13,8 @@ var_disk="${var_disk:-3}"
 var_os="${var_os:-debian}"
 var_version="${var_version:-12}"
 var_unprivileged="${var_unprivileged:-1}"
+var_fuser="${var_fuser:-1}"
+var_tunnel="${var_tunnel:-1}"
 
 header_info "$APP"
 variables
