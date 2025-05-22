563769904f5f82f98002c9dd78b79087ce205e47
CanbiZ <47820557+MickLesk@users.noreply.github.com>
2025-05-22 14:25:39 +0200

    test

--- ct/argus.sh
+++ ct/argus.sh
@@ -44,6 +44,8 @@ function update_script() {
 }
 
 start
+build_container
+description
 
 msg_ok "Completed Successfully!\n"
 echo -e "${CREATING}${GN}${APP} setup has been successfully initialized!${CL}"
