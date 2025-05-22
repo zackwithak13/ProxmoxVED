bf332429b72e29144fb5c2b7c2fdac4e805dfd9d
CanbiZ <47820557+MickLesk@users.noreply.github.com>
2025-05-22 13:57:07 +0200

    Update pulse.sh

--- ct/pulse.sh
+++ ct/pulse.sh
@@ -6,7 +6,7 @@ source <(curl -s https://raw.githubusercontent.com/community-scripts/ProxmoxVED/
 # Source: https://github.com/rcourtman/Pulse
 
 APP="Pulse"
-var_tags="monitoring;nodejs"
+var_tags="monitoring;proxmox"
 var_cpu="1"
 var_ram="1024"
 var_disk="4"
