3e8f4cc4ba970ca9ecccba97ca7d2c5ddd485b85
CanbiZ <47820557+MickLesk@users.noreply.github.com>
2025-05-22 10:32:39 +0200

    Update lyrionmusicserver-install.sh

--- install/lyrionmusicserver-install.sh
+++ install/lyrionmusicserver-install.sh
@@ -13,15 +13,13 @@ setting_up_container
 network_check
 update_os
 
-msg_info "Installing Lyrion Music Server"
-
-DEB_URL=$(curl -s 'https://lyrion.org/getting-started/' | grep -oP '<a\s[^>]*href="\K[^"]*amd64\.deb(?="[^>]*>)' | head -n 1)
-LYRION_VERSION=$(echo "$DEB_URL" | grep -oP 'lyrionmusicserver_\K[0-9.]+(?=_amd64\.deb)')
-DEB_FILE="/tmp/lyrionmusicserver_${LYRION_VERSION}_amd64.deb"
+msg_info "Setup Lyrion Music Server"
+DEB_URL=$(curl -fsSL 'https://lyrion.org/getting-started/' | grep -oP '<a\s[^>]*href="\K[^"]*amd64\.deb(?="[^>]*>)' | head -n 1)
+RELEASE=$(echo "$DEB_URL" | grep -oP 'lyrionmusicserver_\K[0-9.]+(?=_amd64\.deb)')
+DEB_FILE="/tmp/lyrionmusicserver_${RELEASE}_amd64.deb"
 curl -fsSL -o "$DEB_FILE" "$DEB_URL"
 $STD apt install "$DEB_FILE" -y
-
-msg_ok "Installed Lyrion Music Server v${LYRION_VERSION}"
+msg_ok "Setup Lyrion Music Server v${RELEASE}"
 
 motd_ssh
 customize
