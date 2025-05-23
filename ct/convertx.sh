2173432f9754ff369ff88cfb9c8904481c338d3c
Omar <omiinaya@gmail.com>
2025-05-23 00:22:48 -0400

    restarting after updating.

--- ct/convertx.sh
+++ ct/convertx.sh
@@ -30,9 +30,9 @@ function update_script() {
     msg_info "Updating $APP LXC"
     cd /opt/convertx
     git pull origin main
-    $STD systemctl restart convertx.service
     $STD apt-get update
     $STD apt-get -y upgrade
+    $STD systemctl restart convertx.service
     msg_ok "Updated $APP LXC"
     exit
 }
