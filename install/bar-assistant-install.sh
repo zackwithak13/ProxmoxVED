954c58a79dc3f8f0b658739d85be752dfe76c6e9
Bas van den Berg <74251551+bvdberg01@users.noreply.github.com>
2025-05-25 19:34:31 +0200

    Update bar-assistant-install.sh

--- install/bar-assistant-install.sh
+++ install/bar-assistant-install.sh
@@ -170,7 +170,7 @@ server {
     error_page 404 /index.php;
 
     location ~ ^/index\.php(/|$) {
-        fastcgi_pass unix:/var/run/php/php8.4-fpm.sock;
+        fastcgi_pass unix:/var/run/php/php$PHPVER-fpm.sock;
         fastcgi_param SCRIPT_FILENAME \$realpath_root\$fastcgi_script_name;
         include fastcgi_params;
         fastcgi_hide_header X-Powered-By;
