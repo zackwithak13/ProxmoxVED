a1195dbbe81fac3caec430674a01a75cd0d2fb3a
CanbiZ <47820557+MickLesk@users.noreply.github.com>
2025-05-27 14:45:40 +0200

    fix

--- ct/linkwarden.sh
+++ ct/linkwarden.sh
@@ -22,7 +22,7 @@ function update_script() {
   header_info
   check_container_storage
   check_container_resources
-  if [[ ! -d /opt/linkwarden ]]; then
+  if [[ ! -d /opt/Linkwarden ]]; then
     msg_error "No ${APP} Installation Found!"
     exit
   fi
