30f72b6061d126df0a6f55543f96a17e703c5428
CanbiZ <47820557+MickLesk@users.noreply.github.com>
2025-06-05 09:28:49 +0200

    add zot

---
+++ install/zot-install.sh
@@ -0,0 +1,60 @@
+#!/usr/bin/env bash
+
+# Copyright (c) 2021-2025 tteck
+# Author: tteck (tteckster)
+# License: MIT
+# https://github.com/community-scripts/ProxmoxVED/raw/main/LICENSE
+
+source /dev/stdin <<<"$FUNCTIONS_FILE_PATH"
+color
+verb_ip6
+catch_errors
+setting_up_container
+network_check
+update_os
+
+msg_info "Installing Dependencies"
+#$STD apt-get install -y gnup
+msg_ok "Installed Dependencies"
+
+PYTHON_VERSION="3.12" setup_uv
+
+msg_info "Installing Zot"
+RELEASE=$(curl -fsSL https://api.github.com/repos/project-zot/zot/releases/latest | grep "tag_name" | awk '{print substr($2, 2, length($2)-3) }')
+curl -fsSL "https://github.com/project-zot/zot/releases/download/${RELEASE}/zot-linux-amd64" -o /usr/bin/zot
+chmod +x /usr/bin/zot
+chown root:root /usr/bin/zot
+curl -fsSL https://raw.githubusercontent.com/project-zot/zot/refs/heads/main/examples/config-ui.json -o /etc/zot/config.json
+PASSWORD=$(openssl rand -base64 18 | tr -dc 'a-zA-Z0-9' | head -c13)
+$STD htpasswd -cb -B /etc/zot/htpasswd admin "$PASSWORD"
+msg_ok "Installed Zot"
+
+msg_info "Setup Service"
+cat <<EOF >/etc/systemd/system/zot.service
+[Unit]
+Description=OCI Distribution Registry
+Documentation=https://zotregistry.dev/
+After=network.target auditd.service local-fs.target
+
+[Service]
+Type=simple
+ExecStart=/usr/bin/zot serve /etc/zot/config.json
+Restart=on-failure
+User=root
+LimitNOFILE=500000
+MemoryHigh=2G
+MemoryMax=4G
+
+[Install]
+WantedBy=multi-user.target
+EOF
+systemctl enable -q --now zot
+msg_ok "Setup Service"
+
+motd_ssh
+customize
+
+msg_info "Cleaning up"
+$STD apt-get -y autoremove
+$STD apt-get -y autoclean
+msg_ok "Cleaned"
