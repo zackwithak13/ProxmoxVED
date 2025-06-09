12d64041b0035ecf9f2046f977301f6affbda67a
vhsdream <punk.sand7393@fastmail.com>
2025-06-08 20:43:15 -0400

    Maybe: move silent function

--- install/maybefinance-install.sh
+++ install/maybefinance-install.sh
@@ -74,7 +74,7 @@ RAILS_ASSUME_SSL=false' \
 $STD ./bin/bundle install
 $STD ./bin/bundle exec bootsnap precompile --gemfile -j 0
 $STD ./bin/bundle exec bootsnap precompile -j 0 app/ lib/
-$STD SECRET_KEY_BASE_DUMMY=1 ./bin/rails assets:precompile
+SECRET_KEY_BASE_DUMMY=1 $STD ./bin/rails assets:precompile
 $STD dotenv -f ./.env ./bin/rails db:prepare
 echo "${RELEASE}" >/opt/maybe_version.txt
 msg_ok "Installed ${APPLICATION}"
