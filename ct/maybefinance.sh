e9556085d41eba22dec594dd6b4685ebfcfd6410
vhsdream <punk.sand7393@fastmail.com>
2025-06-08 20:18:47 -0400

    New script: Maybe Finance

---
+++ ct/maybefinance.sh
@@ -0,0 +1,79 @@
+#!/usr/bin/env bash
+source <(curl -s https://raw.githubusercontent.com/community-scripts/ProxmoxVED/main/misc/build.func)
+# Copyright (c) 2021-2025 community-scripts ORG
+# Author: vhsdream
+# License: MIT | https://github.com/community-scripts/ProxmoxVED/raw/main/LICENSE
+# Source: https://maybefinance.com
+
+APP="Maybe Finance"
+var_tags="${var_tags:-finance;budget}"
+var_cpu="${var_cpu:-2}"
+var_ram="${var_ram:-2048}"
+var_disk="${var_disk:-4}"
+var_os="${var_os:-debian}"
+var_version="${var_version:-12}"
+var_unprivileged="${var_unprivileged:-1}"
+
+header_info "$APP"
+variables
+color
+catch_errors
+
+function update_script() {
+  header_info
+  check_container_storage
+  check_container_resources
+
+  if [[ ! -d /opt/maybe ]]; then
+    msg_error "No ${APP} Installation Found!"
+    exit
+  fi
+
+  RELEASE=$(curl -s https://api.github.com/repos/maybe-finance/maybe/releases/latest | grep "tag_name" | awk '{print substr($2, 3, length($2)-4) }')
+  if [[ "${RELEASE}" != "$(cat /opt/maybe_version.txt)" ]] || [[ ! -f /opt/maybe_version.txt ]]; then
+    msg_info "Stopping $APP"
+    systemctl stop maybe-web maybe-worker
+    msg_ok "Stopped $APP"
+
+    msg_info "Creating Backup"
+    cp /opt/maybe/.env /opt/maybe.env
+    msg_ok "Backup Created"
+
+    msg_info "Updating $APP to v${RELEASE}"
+    rm -rf /opt/maybe
+    curl -fsSL "https://github.com/maybe-finance/maybe/archive/refs/tags/v${RELEASE}.zip" -o /tmp/v"$RELEASE".zip
+    unzip -q /tmp/v"$RELEASE".zip
+    mv maybe-"$RELEASE" /opt/maybe
+    cd /opt/maybe
+    source ~/.profile
+    $STD ./bin/bundle install
+    $STD ./bin/bundle exec bootsnap precompile --gemfile -j 0
+    $STD ./bin/bundle exec bootsnap precompile -j 0 app/ lib/
+    $STD SECRET_KEY_BASE_DUMMY=1 ./bin/rails assets:precompile
+    $STD dotenv -f ./.env ./bin/rails db:prepare
+    msg_ok "Updated $APP to v${RELEASE}"
+
+    msg_info "Starting $APP"
+    systemctl start maybe-worker maybe-web
+    msg_ok "Started $APP"
+
+    msg_info "Cleaning Up"
+    rm /tmp/v"$RELEASE".zip
+    msg_ok "Cleanup Completed"
+
+    echo "${RELEASE}" >/opt/maybe_version.txt
+    msg_ok "Update Successful"
+  else
+    msg_ok "No update required. ${APP} is already at v${RELEASE}"
+  fi
+  exit
+}
+
+start
+build_container
+description
+
+msg_ok "Completed Successfully!\n"
+echo -e "${CREATING}${GN}${APP} setup has been successfully initialized!${CL}"
+echo -e "${INFO}${YW} Access it using the following URL:${CL}"
+echo -e "${TAB}${GATEWAY}${BGN}http://${IP}:3000${CL}"
---
+++ install/maybefinance-install.sh
@@ -0,0 +1,124 @@
+#!/usr/bin/env bash
+
+# Copyright (c) 2021-2025 community-scripts ORG
+# Author: vhsdream
+# License: MIT | https://github.com/community-scripts/ProxmoxVED/raw/main/LICENSE
+# Source: https://maybefinance.com
+
+source /dev/stdin <<<"$FUNCTIONS_FILE_PATH"
+color
+verb_ip6
+catch_errors
+setting_up_container
+network_check
+update_os
+
+msg_info "Installing Dependencies"
+$STD apt-get install -y --no-install-recommends \
+  libpq-dev \
+  libvips42 \
+  git \
+  zlib1g-dev \
+  build-essential \
+  libssl-dev \
+  libreadline-dev \
+  libyaml-dev \
+  libsqlite3-dev \
+  sqlite3 \
+  libxml2-dev \
+  libxslt1-dev \
+  libcurl4-openssl-dev \
+  software-properties-common \
+  libffi-dev \
+  redis
+msg_ok "Installed Dependencies"
+
+PG_VERSION=16 install_postgresql
+RUBY_VERSION=3.4.1 RUBY_INSTALL_RAILS=false setup_rbenv_stack
+
+msg_info "Setting up Postgresql"
+DB_NAME="maybe"
+DB_USER="maybe"
+DB_PASS="$(openssl rand -base64 18 | tr -dc 'a-zA-Z0-9' | head -c13)"
+$STD sudo -u postgres psql -c "CREATE ROLE $DB_USER WITH LOGIN PASSWORD '$DB_PASS';"
+$STD sudo -u postgres psql -c "CREATE DATABASE $DB_NAME WITH OWNER $DB_USER TEMPLATE template0;"
+{
+  echo "${APPLICATION} database credentials"
+  echo "Database Name: ${DB_NAME}"
+  echo "Database User: ${DB_USER}"
+  echo "Database Password: ${DB_PASS}"
+} >~/maybe.creds
+msg_ok "Setup Postgresql"
+
+msg_info "Installing ${APPLICATION}"
+RELEASE=$(curl -s https://api.github.com/repos/maybe-finance/maybe/releases/latest | grep "tag_name" | awk '{print substr($2, 3, length($2)-4) }')
+curl -fsSL "https://github.com/maybe-finance/maybe/archive/refs/tags/v${RELEASE}.zip" -o /tmp/v"$RELEASE".zip
+unzip -q /tmp/v"$RELEASE".zip
+mv maybe-"$RELEASE" /opt/maybe
+cd /opt/maybe
+cp ./.env.example ./.env
+sed -i -e "s/secret-value/\"$(openssl rand -hex 64)\"/" \
+  -e "/^SECRET_KEY/a RAILS_MASTER_KEY=\"$(openssl rand -hex 16)\"" \
+  -e "s/_USER=postgres/_USER=${DB_USER}/" \
+  -e "s/_PASSWORD=postgres/_PASSWORD=${DB_PASS}/" \
+  -e "/_USER=/a POSTGRES_DB=${DB_NAME}" \
+  -e 's/^# DISABLE/DISABLE/' \
+  ./.env
+sed -i -e '/_DB=/a\
+\
+REDIS_URL=http://127.0.0.1:6379' \
+  -e '/_SSL/a\
+RAILS_FORCE_SSL=false\
+RAILS_ASSUME_SSL=false' \
+  ./.env
+$STD ./bin/bundle install
+$STD ./bin/bundle exec bootsnap precompile --gemfile -j 0
+$STD ./bin/bundle exec bootsnap precompile -j 0 app/ lib/
+$STD SECRET_KEY_BASE_DUMMY=1 ./bin/rails assets:precompile
+$STD dotenv -f ./.env ./bin/rails db:prepare
+echo "${RELEASE}" >/opt/maybe_version.txt
+msg_ok "Installed ${APPLICATION}"
+
+msg_info "Creating services"
+
+cat <<EOF >/etc/systemd/system/maybe-web.service
+[Unit]
+Description=${APPLICATION} Web Service
+After=network.target redis.service postgresql.service
+
+[Service]
+Type=simple
+WorkingDirectory=/opt/maybe
+ExecStart=/root/.rbenv/shims/dotenv -f /opt/maybe/.env /opt/maybe/bin/rails s
+Restart=unless-stopped
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+cat <<EOF >/etc/systemd/system/maybe-worker.service
+[Unit]
+Description=${APPLICATION} Worker Service
+After=redis.service
+
+[Service]
+Type=simple
+WorkingDirectory=/opt/maybe
+EnvironmentFile=/opt/maybe/.env
+ExecStart=bundle exec sidekiq
+Restart=unless-stopped
+
+[Install]
+WantedBy=multi-user.target
+EOF
+systemctl enable -q --now maybe-web maybe-worker
+msg_ok "Created services"
+
+motd_ssh
+customize
+
+msg_info "Cleaning up"
+rm -f /tmp/v"$RELEASE".zip
+$STD apt-get -y autoremove
+$STD apt-get -y autoclean
+msg_ok "Cleaned"
