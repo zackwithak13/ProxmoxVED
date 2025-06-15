e2af2459ebb399f2f29c23d7fc06e9daa2c8024f
tremor021 <arezina.slavisa@gmail.com>
2025-06-15 22:10:47 +0200

    Update Firefly

--- install/firefly-install.sh
+++ install/firefly-install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Copyright (c) 2021-2025 community-scripts ORG
-# Author: quantumryuu
+# Author: quantumryuu | Co-Author: Slaviša Arežina (tremor021)
 # License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
 # Source: https://firefly-iii.org/
 
