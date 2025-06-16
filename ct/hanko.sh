3f9d588722c1d0f20e562b8709fbbedede4c79fc
CanbiZ <47820557+MickLesk@users.noreply.github.com>
2025-06-16 14:44:50 +0200

    Update hanko.sh

--- ct/hanko.sh
+++ ct/hanko.sh
@@ -5,16 +5,14 @@ source <(curl -s https://git.community-scripts.org/community-scripts/ProxmoxVED/
 # License: MIT | https://github.com/community-scripts/ProxmoxVED/raw/main/LICENSE
 # Source: https://www.debian.org/
 
-APP="Debian"
+APP="Hanko"
 var_tags="${var_tags:-os}"
 var_cpu="${var_cpu:-1}"
-var_ram="${var_ram:-512}"
-var_disk="${var_disk:-2}"
+var_ram="${var_ram:-1024}"
+var_disk="${var_disk:-4}"
 var_os="${var_os:-debian}"
 var_version="${var_version:-12}"
 var_unprivileged="${var_unprivileged:-1}"
-var_fuse="${var_fuse:-yes}"
-var_tun="${var_tun:-yes}"
 
 header_info "$APP"
 variables
@@ -42,3 +40,5 @@ description
 
 msg_ok "Completed Successfully!\n"
 echo -e "${CREATING}${GN}${APP} setup has been successfully initialized!${CL}"
+echo -e "${INFO}${YW} Access it using the following URL:${CL}"
+echo -e "${TAB}${GATEWAY}${BGN}http://${IP}:8000${CL}"
