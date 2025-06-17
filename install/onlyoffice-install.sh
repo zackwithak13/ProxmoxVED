51c51f1f36eedd65d4688ca1301d01d0c84ba8c7
CanbiZ <47820557+MickLesk@users.noreply.github.com>
2025-06-17 09:38:28 +0200

    fixes

---
+++ install/onlyoffice-install.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+# Copyright (c) 2021-2025 community-scripts ORG
+# Author: MickLesk (CanbiZ)
+# License: MIT | https://github.com/community-scripts/ProxmoxVED/raw/main/LICENSE
+
+source /dev/stdin <<<"$FUNCTIONS_FILE_PATH"
+color
+verb_ip6
+catch_errors
+setting_up_container
+network_check
+update_os
+
+msg_info "Installing Dependencies"
+$STD apt-get install -y \
+  nginx \
+  rabbitmq-server
+msg_ok "Installed Dependencies"
+
+PG_VERSION="16" setup_postgresql
+
+msg_info "Setup Database"
+DB_NAME=onlyoffice
+DB_USER=onlyoffice_user
+DB_PASS=$(openssl rand -base64 18 | tr -dc 'a-zA-Z0-9' | cut -c1-13)
+$STD sudo -u postgres psql -c "CREATE ROLE $DB_USER WITH LOGIN PASSWORD '$DB_PASS';"
+$STD sudo -u postgres psql -c "CREATE DATABASE $DB_NAME WITH OWNER $DB_USER ENCODING 'UTF8' TEMPLATE template0;"
+$STD sudo -u postgres psql -c "ALTER ROLE $DB_USER SET client_encoding TO 'utf8';"
+$STD sudo -u postgres psql -c "ALTER ROLE $DB_USER SET default_transaction_isolation TO 'read committed';"
+$STD sudo -u postgres psql -c "ALTER ROLE $DB_USER SET timezone TO 'UTC'"
+{
+  echo "OnlyOffice-Credentials"
+  echo "OnlyOffice Database User: $DB_USER"
+  echo "OnlyOffice Database Password: $DB_PASS"
+  echo "OnlyOffice Database Name: $DB_NAME"
+} >>~/onlyoffice.creds
+msg_ok "Set up Database"
+
+motd_ssh
+customize
+
+msg_info "Cleaning up"
+$STD apt-get -y autoremove
+$STD apt-get -y autoclean
+msg_ok "Cleaned"
--- misc/create_lxc.sh
+++ misc/create_lxc.sh
@@ -144,10 +144,12 @@ if [ "$STORAGE_FREE" -lt "$REQUIRED_KB" ]; then
 fi
 
 # Check Cluster Quorum if in Cluster
-if ! pvecm status | awk '/^Quorate:/ {exit $2 != "Yes"}'; then
-  printf "\e[?25h"
-  echo -e "\n${CROSS}${RD}Cluster is not quorate. Start all nodes or configure quorum device (QDevice).${CL}\n"
-  exit 210
+if [ -f /etc/pve/corosync.conf ]; then
+  if ! pvecm status | grep -q "Quorate: Yes"; then
+    printf "\e[?25h"
+    echo -e "\n${CROSS}${RD}Cluster is not quorate. Start all nodes or configure quorum device (QDevice).${CL}\n"
+    exit 210
+  fi
 fi
 
 # Update LXC template list
