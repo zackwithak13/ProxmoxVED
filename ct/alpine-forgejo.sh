b8b9a2ed8ed5384531b202905e1cc2583d064463
Hannes <johanneshaegele@gmx.net>
2025-06-18 10:39:46 +0200

    Alpine: Forgejo

---
+++ ct/alpine-forgejo.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+source ./build.func
+# Copyright (c) 2021-2025 community-scripts ORG
+# Author: Johann3s-H (An!ma)
+# License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
+# Source: https://forgejo.org/
+
+APP="Alpine-Forgejo"
+var_tags="${var_tags:-alpine;git}"
+var_cpu="${var_cpu:-1}"
+var_ram="${var_ram:-256}"
+var_disk="${var_disk:-1}"
+var_os="${var_os:-alpine}"
+var_version="${var_version:-3.21}"
+var_unprivileged="${var_unprivileged:-1}"
+
+header_info "$APP"
+variables
+color
+catch_errors
+
+function update_script() {
+  header_info
+
+  msg_info "Updating Alpine Packages"
+  $STD apk -U upgrade
+  msg_ok "Updated Alpine Packages"
+
+  msg_info "Updating Forgejo"
+  apk upgrade forgejo
+  msg_ok "Updated Forgejo"
+
+  msg_info "Restarting Forgejo"
+  rc-service forgejo restart
+  msg_ok "Restarted Forgejo"
+}
+
+start
+msg_info "start done"
+build_container
+msg_info "build_container done"
+description
+msg_info "description done"
+
+msg_ok "Completed Successfully!\n"
+echo -e "${CREATING}${GN}${APP} setup has been successfully initialized!${CL}"
+echo -e "${INFO}${YW} Access it using the following URL:${CL}"
+echo -e "${TAB}${GATEWAY}${BGN}http://${IP}:3000${CL}"
