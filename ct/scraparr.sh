16bea4814848dbad28cbc4c41ae9cac4436a6db4
Jason Green <jason.green@perfectday.com>
2025-06-27 09:04:12 -0700

    fixed build links for dev

--- ct/scraparr.sh
+++ ct/scraparr.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-source <(curl -fsSL https://raw.githubusercontent.com/JasonGreenC/ProxmoxVED/refs/head/scraparr/misc/build.func)
+source <(curl -fsSL https://raw.githubusercontent.com/JasonGreenC/ProxmoxVED/refs/head/scraparr-uv-migration/misc/build.func)
 # Copyright (c) 2021-2025 community-scripts ORG
 # Author: JasonGreenC
 # License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
