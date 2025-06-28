5e5f22f8a7b44363b332e9e845e6722711ca0526
Jason Green <jason.green@perfectday.com>
2025-06-27 17:42:38 -0700

    Added completation message

--- ct/scraparr.sh
+++ ct/scraparr.sh
@@ -60,6 +60,7 @@ start
 build_container
 description
 
+msg_ok "Completed Successfully!\n"
 echo -e "${CREATING}${GN}${APP} setup has been successfully initialized!${CL}"
 echo -e "${INFO}${YW} Access it using the following URL:${CL}"
 echo -e "${TAB}${GATEWAY}${BGN}http://${IP}:7100${CL}"
