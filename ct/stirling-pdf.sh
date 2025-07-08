edebef124379b2d5bb7478759b787abba3584c24
CanbiZ <47820557+MickLesk@users.noreply.github.com>
2025-07-08 07:59:03 +0200

    cleanup

---
+++ ct/stirling-pdf.sh
@@ -0,0 +1,62 @@
+#!/usr/bin/env bash
+source <(curl -fsSL https://raw.githubusercontent.com/community-scripts/ProxmoxVED/main/misc/build.func)
+# Copyright (c) 2021-2025 tteck
+# Author: tteck (tteckster)
+# License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
+# Source: https://www.stirlingpdf.com/
+
+APP="Stirling-PDF"
+var_tags="${var_tags:-pdf-editor}"
+var_cpu="${var_cpu:-2}"
+var_ram="${var_ram:-2048}"
+var_disk="${var_disk:-8}"
+var_os="${var_os:-debian}"
+var_version="${var_version:-12}"
+var_unprivileged="${var_unprivileged:-1}"
+
+header_info "$APP"
+variables
+color
+catch_errors
+
+function update_script() {
+  header_info
+  check_container_storage
+  check_container_resources
+  if [[ ! -d /opt/Stirling-PDF ]]; then
+    msg_error "No ${APP} Installation Found!"
+    exit
+  fi
+  msg_info "Updating ${APP}"
+  systemctl stop stirlingpdf
+  if [[ -n $(dpkg -l | grep -w ocrmypdf) ]] && [[ -z $(dpkg -l | grep -w qpdf) ]]; then
+    $STD apt-get remove -y ocrmypdf
+    $STD apt-get install -y qpdf
+  fi
+  RELEASE=$(curl -fsSL https://api.github.com/repos/Stirling-Tools/Stirling-PDF/releases/latest | grep "tag_name" | awk '{print substr($2, 3, length($2)-4) }')
+  curl -fsSL "https://github.com/Stirling-Tools/Stirling-PDF/archive/refs/tags/v$RELEASE.tar.gz" -o $(basename "https://github.com/Stirling-Tools/Stirling-PDF/archive/refs/tags/v$RELEASE.tar.gz")
+  tar -xzf v$RELEASE.tar.gz
+  cd Stirling-PDF-$RELEASE
+  chmod +x ./gradlew
+  $STD ./gradlew build -x spotlessApply -x spotlessCheck -x test -x sonarqube
+  rm -rf /opt/Stirling-PDF/Stirling-PDF-*.jar
+  cp -r ./stirling-pdf/build/libs/*.jar /opt/Stirling-PDF/Stirling-PDF-$RELEASE.jar
+  cp -r scripts /opt/Stirling-PDF/
+  cp -r pipeline /opt/Stirling-PDF/
+  cp -r stirling-pdf/src/main/resources/static/fonts/*.ttf /usr/share/fonts/opentype/noto/
+  cd ~
+  rm -rf Stirling-PDF-$RELEASE v$RELEASE.tar.gz
+  ln -sf /opt/Stirling-PDF/Stirling-PDF-$RELEASE.jar /opt/Stirling-PDF/Stirling-PDF.jar
+  systemctl start stirlingpdf
+  msg_ok "Updated ${APP} to v$RELEASE"
+  exit
+}
+
+start
+build_container
+description
+
+msg_ok "Completed Successfully!\n"
+echo -e "${CREATING}${GN}${APP} setup has been successfully initialized!${CL}"
+echo -e "${INFO}${YW} Access it using the following URL:${CL}"
+echo -e "${TAB}${GATEWAY}${BGN}http://${IP}:8080${CL}"
---
+++ install/stirling-pdf-install.sh
@@ -0,0 +1,154 @@
+#!/usr/bin/env bash
+
+# Copyright (c) 2021-2025 tteck
+# Author: tteck (tteckster)
+# License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
+# Source: https://www.stirlingpdf.com/
+
+source /dev/stdin <<<"$FUNCTIONS_FILE_PATH"
+color
+verb_ip6
+catch_errors
+setting_up_container
+network_check
+update_os
+
+msg_info "Installing Dependencies (Patience)"
+$STD apt-get install -y \
+  git \
+  automake \
+  autoconf \
+  libtool \
+  libleptonica-dev \
+  pkg-config \
+  zlib1g-dev \
+  make \
+  g++ \
+  unpaper \
+  qpdf \
+  poppler-utils
+msg_ok "Installed Dependencies"
+
+msg_info "Installing LibreOffice Components"
+$STD apt-get install -y \
+  libreoffice-writer \
+  libreoffice-calc \
+  libreoffice-impress \
+  libreoffice-core \
+  libreoffice-common \
+  libreoffice-base-core \
+  python3-uno
+msg_ok "Installed LibreOffice Components"
+
+msg_info "Installing Python Dependencies"
+$STD apt-get install -y \
+  python3 \
+  python3-pip
+rm -rf /usr/lib/python3.*/EXTERNALLY-MANAGED
+$STD pip3 install \
+  uno \
+  opencv-python-headless \
+  unoconv \
+  pngquant \
+  WeasyPrint
+msg_ok "Installed Python Dependencies"
+
+msg_info "Installing Azul Zulu"
+curl -fsSL "https://keyserver.ubuntu.com/pks/lookup?op=get&search=0xB1998361219BD9C9" -o "/etc/apt/trusted.gpg.d/zulu-repo.asc"
+curl -fsSL "https://cdn.azul.com/zulu/bin/zulu-repo_1.0.0-3_all.deb" -o "/zulu-repo_1.0.0-3_all.deb"
+$STD dpkg -i zulu-repo_1.0.0-3_all.deb
+$STD apt-get update
+$STD apt-get -y install zulu17-jdk
+msg_ok "Installed Azul Zulu"
+
+msg_info "Installing JBIG2"
+$STD git clone https://github.com/agl/jbig2enc /opt/jbig2enc
+cd /opt/jbig2enc
+$STD bash ./autogen.sh
+$STD bash ./configure
+$STD make
+$STD make install
+msg_ok "Installed JBIG2"
+
+msg_info "Installing Language Packs (Patience)"
+$STD apt-get install -y 'tesseract-ocr-*'
+msg_ok "Installed Language Packs"
+
+msg_info "Installing Stirling-PDF (Additional Patience)"
+RELEASE=$(curl -fsSL https://api.github.com/repos/Stirling-Tools/Stirling-PDF/releases/latest | grep "tag_name" | awk '{print substr($2, 3, length($2)-4) }')
+curl -fsSL "https://github.com/Stirling-Tools/Stirling-PDF/archive/refs/tags/v${RELEASE}.tar.gz" -o "v${RELEASE}.tar.gz"
+tar -xzf v${RELEASE}.tar.gz
+cd Stirling-PDF-$RELEASE
+chmod +x ./gradlew
+$STD ./gradlew build -x spotlessApply -x spotlessCheck -x test -x sonarqube
+mkdir -p /opt/Stirling-PDF
+touch /opt/Stirling-PDF/.env
+mv ./stirling-pdf/build/libs/*.jar /opt/Stirling-PDF/Stirling-PDF-$RELEASE.jar
+mv scripts /opt/Stirling-PDF/
+mv pipeline /opt/Stirling-PDF/
+mv stirling-pdf/src/main/resources/static/fonts/*.ttf /usr/share/fonts/opentype/noto/
+ln -s /opt/Stirling-PDF/Stirling-PDF-$RELEASE.jar /opt/Stirling-PDF/Stirling-PDF.jar
+ln -s /usr/share/tesseract-ocr/5/tessdata/ /usr/share/tessdata
+msg_ok "Installed Stirling-PDF"
+
+msg_info "Creating Service"
+# Create LibreOffice listener service
+cat <<EOF >/etc/systemd/system/libreoffice-listener.service
+[Unit]
+Description=LibreOffice Headless Listener Service
+After=network.target
+
+[Service]
+Type=simple
+User=root
+Group=root
+ExecStart=/usr/lib/libreoffice/program/soffice --headless --invisible --nodefault --nofirststartwizard --nolockcheck --nologo --accept="socket,host=127.0.0.1,port=2002;urp;StarOffice.ComponentContext"
+Restart=always
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# Set up environment variables
+cat <<EOF >/opt/Stirling-PDF/.env
+PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/lib/libreoffice/program
+UNO_PATH=/usr/lib/libreoffice/program
+PYTHONPATH=/usr/lib/python3/dist-packages:/usr/lib/libreoffice/program
+LD_LIBRARY_PATH=/usr/lib/libreoffice/program
+EOF
+
+cat <<EOF >/etc/systemd/system/stirlingpdf.service
+[Unit]
+Description=Stirling-PDF service
+After=syslog.target network.target libreoffice-listener.service
+Requires=libreoffice-listener.service
+
+[Service]
+SuccessExitStatus=143
+Type=simple
+User=root
+Group=root
+EnvironmentFile=/opt/Stirling-PDF/.env
+WorkingDirectory=/opt/Stirling-PDF
+ExecStart=/usr/bin/java -jar Stirling-PDF.jar
+ExecStop=/bin/kill -15 %n
+Restart=always
+RestartSec=10
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# Enable and start services
+systemctl enable -q --now libreoffice-listener
+systemctl enable -q --now stirlingpdf
+msg_ok "Created Service"
+
+motd_ssh
+customize
+
+msg_info "Cleaning up"
+rm -rf v${RELEASE}.tar.gz /zulu-repo_1.0.0-3_all.deb
+$STD apt-get -y autoremove
+$STD apt-get -y autoclean
+msg_ok "Cleaned"
