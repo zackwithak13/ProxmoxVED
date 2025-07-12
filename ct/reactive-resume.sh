ac60b04ea5ec5ecd5e46c21d36a3a7982e77b131
vhsdream <punk.sand7393@fastmail.com>
2025-07-12 10:09:05 -0400

    Reactive-Resume: Update source

--- ct/reactive-resume.sh
+++ ct/reactive-resume.sh
@@ -3,7 +3,7 @@ source <(curl -fsSL https://raw.githubusercontent.com/community-scripts/ProxmoxV
 # Copyright (c) 2021-2025 community-scripts ORG
 # Author: vhsdream
 # License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
-# Source: https://rxresu.me
+# Source: https://rxresume.org
 
 APP="Reactive-Resume"
 var_tags="${var_tags:-documents}"
--- install/reactive-resume-install.sh
+++ install/reactive-resume-install.sh
@@ -3,7 +3,7 @@
 # Copyright (c) 2021-2025 community-scripts ORG
 # Author: vhsdream
 # License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
-# Source: https://rxresu.me
+# Source: https://rxresume.org
 
 source /dev/stdin <<<"$FUNCTIONS_FILE_PATH"
 color
