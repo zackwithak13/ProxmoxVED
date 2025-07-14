3c624f97b39cf564dad0841b8113981a592bec8e
CanbiZ <47820557+MickLesk@users.noreply.github.com>
2025-07-14 12:52:03 +0200

    Update rybbit-install.sh

--- install/rybbit-install.sh
+++ install/rybbit-install.sh
@@ -20,7 +20,7 @@ $STD apt-get install -y \
   ca-certificates
 msg_ok "Installed Dependencies"
 
-CLICKHOUSE_DB="rybbit_db" CLICKHOUSE_USER="rybbit" setup_clickhouse
+setup_clickhouse
 PG_VERSION=17 setup_postgresql
 NODE_VERSION="20" NODE_MODULE="next" setup_nodejs
 
