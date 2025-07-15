184839f96140c5254a25c55bb83fce05ffbbade5
CanbiZ <47820557+MickLesk@users.noreply.github.com>
2025-07-15 13:08:29 +0200

    test

---
+++ install/nginxproxymanager-install.sh
@@ -0,0 +1,171 @@
+#!/usr/bin/env bash
+
+# Copyright (c) 2021-2025 tteck
+# Author: tteck (tteckster)
+# License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
+# Source: https://nginxproxymanager.com/
+
+source /dev/stdin <<<"$FUNCTIONS_FILE_PATH"
+color
+verb_ip6
+catch_errors
+setting_up_container
+network_check
+update_os
+
+msg_info "Installing Dependencies"
+$STD apt-get update
+$STD apt-get -y install \
+  ca-certificates \
+  apache2-utils \
+  logrotate \
+  build-essential \
+  jq \
+  git
+msg_ok "Installed Dependencies"
+
+NODE_VERSION="20" NODE_MODULE="yarn" NODE_OPTIONS="--openssl-legacy-provider" setup_nodejs
+PYTHON_VERSION="3.12" setup_uv
+fetch_and_deploy_gh_release "nginxproxymanager" "NginxProxyManager/nginx-proxy-manager" "tarball" "latest" "/tmp/nginxproxymanager"
+
+msg_info "Installing Python Dependencies"
+$STD apt-get install -y \
+  python3 \
+  python3-dev \
+  python3-venv
+msg_ok "Installed Python Dependencies"
+
+msg_info "Setting up Certbot Environment"
+$STD uv venv /opt/certbot --python "$PYTHON_VERSION"
+/opt/certbot/bin/uv pip install \
+  certbot \
+  certbot-dns-cloudflare \
+  certbot-dns-multi
+msg_ok "Certbot Environment Ready"
+
+msg_info "Installing Openresty"
+VERSION="$(awk -F'=' '/^VERSION_CODENAME=/{ print $NF }' /etc/os-release)"
+curl -fsSL "https://openresty.org/package/pubkey.gpg" | gpg --dearmor -o /etc/apt/trusted.gpg.d/openresty-archive-keyring.gpg
+echo -e "deb http://openresty.org/package/debian $VERSION openresty" >/etc/apt/sources.list.d/openresty.list
+$STD apt-get update
+$STD apt-get -y install openresty
+msg_ok "Installed Openresty"
+
+msg_info "Setting up Environment"
+ln -sf /usr/bin/python3 /usr/bin/python
+ln -sf /opt/certbot/bin/certbot /usr/bin/certbot
+ln -sf /usr/local/openresty/nginx/sbin/nginx /usr/sbin/nginx
+ln -sf /usr/local/openresty/nginx/ /etc/nginx
+sed -i 's+^daemon+#daemon+g' docker/rootfs/etc/nginx/nginx.conf
+NGINX_CONFS=$(find "$(pwd)" -type f -name "*.conf")
+for NGINX_CONF in $NGINX_CONFS; do
+  sed -i 's+include conf.d+include /etc/nginx/conf.d+g' "$NGINX_CONF"
+done
+
+mkdir -p /var/www/html /etc/nginx/logs
+cd /tmp/nginxproxymanager
+cp -r docker/rootfs/var/www/html/* /var/www/html/
+cp -r docker/rootfs/etc/nginx/* /etc/nginx/
+cp docker/rootfs/etc/letsencrypt.ini /etc/letsencrypt.ini
+cp docker/rootfs/etc/logrotate.d/nginx-proxy-manager /etc/logrotate.d/nginx-proxy-manager
+ln -sf /etc/nginx/nginx.conf /etc/nginx/conf/nginx.conf
+rm -f /etc/nginx/conf.d/dev.conf
+
+mkdir -p /tmp/nginx/body \
+  /run/nginx \
+  /data/nginx \
+  /data/custom_ssl \
+  /data/logs \
+  /data/access \
+  /data/nginx/default_host \
+  /data/nginx/default_www \
+  /data/nginx/proxy_host \
+  /data/nginx/redirection_host \
+  /data/nginx/stream \
+  /data/nginx/dead_host \
+  /data/nginx/temp \
+  /var/lib/nginx/cache/public \
+  /var/lib/nginx/cache/private \
+  /var/cache/nginx/proxy_temp
+
+chmod -R 777 /var/cache/nginx
+chown root /tmp/nginx
+
+echo resolver "$(awk 'BEGIN{ORS=" "} $1=="nameserver" {print ($2 ~ ":")? "["$2"]": $2}' /etc/resolv.conf);" >/etc/nginx/conf.d/include/resolvers.conf
+
+if [ ! -f /data/nginx/dummycert.pem ] || [ ! -f /data/nginx/dummykey.pem ]; then
+  openssl req -new -newkey rsa:2048 -days 3650 -nodes -x509 -subj "/O=Nginx Proxy Manager/OU=Dummy Certificate/CN=localhost" -keyout /data/nginx/dummykey.pem -out /data/nginx/dummycert.pem &>/dev/null
+fi
+
+mkdir -p /app/global /app/frontend/images
+cd /tmp/nginxproxymanager
+cp -r backend/* /app
+cp -r global/* /app/global
+msg_ok "Set up Environment"
+
+msg_info "Building Frontend"
+cd /tmp/nginxproxymanager/frontend
+$STD yarn install --frozen-lockfile
+$STD yarn build
+cp -r dist/* /app/frontend
+cp -r app-images/* /app/frontend/images
+msg_ok "Built Frontend"
+
+msg_info "Initializing Backend"
+rm -rf /app/config/default.json
+if [ ! -f /app/config/production.json ]; then
+  cat <<'EOF' >/app/config/production.json
+{
+  "database": {
+    "engine": "knex-native",
+    "knex": {
+      "client": "sqlite3",
+      "connection": {
+        "filename": "/data/database.sqlite"
+      }
+    }
+  }
+}
+EOF
+fi
+cd /app
+$STD yarn install --production
+msg_ok "Initialized Backend"
+
+msg_info "Creating Service"
+cat <<'EOF' >/lib/systemd/system/npm.service
+[Unit]
+Description=Nginx Proxy Manager
+After=network.target
+Wants=openresty.service
+
+[Service]
+Type=simple
+Environment=NODE_ENV=production
+Environment=NODE_OPTIONS=--openssl-legacy-provider
+ExecStartPre=-mkdir -p /tmp/nginx/body /data/letsencrypt-acme-challenge
+ExecStart=/usr/bin/node index.js --abort_on_uncaught_exception --max_old_space_size=1024
+WorkingDirectory=/app
+Restart=on-failure
+
+[Install]
+WantedBy=multi-user.target
+EOF
+msg_ok "Created Service"
+
+motd_ssh
+customize
+
+msg_info "Starting Services"
+sed -i 's/user npm/user root/g; s/^pid/#pid/g' /usr/local/openresty/nginx/conf/nginx.conf
+sed -r -i 's/^([[:space:]]*)su npm npm/\1#su npm npm/g;' /etc/logrotate.d/nginx-proxy-manager
+systemctl enable -q --now openresty
+systemctl enable -q --now npm
+msg_ok "Started Services"
+
+msg_info "Cleaning up"
+rm -rf /tmp/*
+systemctl restart openresty
+$STD apt-get -y autoremove
+$STD apt-get -y autoclean
+msg_ok "Cleaned"
