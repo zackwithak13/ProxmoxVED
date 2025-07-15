a8b2d21905d24f23ae20eccb62c6345d4b3d341a
CanbiZ <47820557+MickLesk@users.noreply.github.com>
2025-07-15 14:01:46 +0200

    test

--- install/debian-install.sh
+++ install/debian-install.sh
@@ -17,7 +17,7 @@ msg_info "Installing Dependencies"
 $STD apt-get install -y gpg
 msg_ok "Installed Dependencies"
 
-setup_mariadb
+#setup_mariadb
 
 #FFMPEG_VERSION="n7.1.1" FFMPEG_TYPE="full" setup_ffmpeg
 
