ea6bffa79f0eea9d5bbf34c5d6220d5cf69bb580
Bas van den Berg <74251551+bvdberg01@users.noreply.github.com>
2025-07-20 13:23:52 +0200

    Update ots-install.sh

--- install/ots-install.sh
+++ install/ots-install.sh
@@ -15,9 +15,7 @@ update_os
 
 msg_info "Installing Dependencies"
 $STD apt-get install -y \
-  caddy \
-  apt-transport-https \
-  ca-certificates
+  redis-server
 msg_ok "Installed Dependencies"
 
 msg_info "Installing OTS"
