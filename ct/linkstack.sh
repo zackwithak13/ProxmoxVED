7cf8f67f6e9c0d4cb3ee489fe2e319d319508f86
CanbiZ <47820557+MickLesk@users.noreply.github.com>
2025-07-22 10:11:08 +0200

    Update linkstack.sh

--- ct/linkstack.sh
+++ ct/linkstack.sh
@@ -28,37 +28,8 @@ function update_script() {
     msg_error "No ${APP} Installation Found!"
     exit
   fi
-
-  RELEASE=$(curl -fsSL https://api.github.com/repos/linkstackorg/linkstack/releases/latest | jq -r '.tag_name | ltrimstr("v")')
-  if [[ "${RELEASE}" != "$(cat ~/.linkstack 2>/dev/null)" ]] || [[ ! -f ~/.linkstack ]]; then
-    msg_info "Stopping $APP"
-    systemctl stop apache2
-    msg_ok "Stopped $APP"
-
-    msg_info "Creating Backup"
-    BACKUP_FILE="/opt/linkstack_backup_$(date +%F).tar.gz"
-    $STD tar -czf "$BACKUP_FILE" /var/www/html/linkstack
-    msg_ok "Backup Created"
-
-    PHP_VERSION="8.3" PHP_MODULE="sqlite3" PHP_APACHE="YES" setup_php
-    fetch_and_deploy_gh_release "linkstack" "linkstackorg/linkstack" "prebuild" "latest" "/var/www/html/linkstack" "linkstack.zip"
-
-    msg_info "Updating $APP to v${RELEASE}"
-    chown -R www-data:www-data /var/www/html/linkstack
-    chmod -R 755 /var/www/html/linkstack
-    msg_ok "Updated $APP to v${RELEASE}"
-
-    msg_info "Starting $APP"
-    systemctl start apache2
-    msg_ok "Started $APP"
-
-    msg_info "Cleaning Up"
-    rm -rf "$BACKUP_FILE"
-    msg_ok "Cleanup Completed"
-    msg_ok "Update Successful"
-  else
-    msg_ok "No update required. ${APP} is already at v${RELEASE}"
-  fi
+  PHP_VERSION="8.3" PHP_MODULE="sqlite3" PHP_APACHE="YES" setup_php
+  msg_error "Adguard Home should be updated via the user interface."
   exit
 }
 
