e3006ecbf3569af9f3be0f97694503021be6b67e
tremor021 <arezina.slavisa@gmail.com>
2025-07-24 13:57:38 +0200

    Update TServer

--- install/alpine-teamspeak-server-install.sh
+++ install/alpine-teamspeak-server-install.sh
@@ -20,7 +20,7 @@ $STD apk add --no-cache \
   libc6-compat
 msg_ok "Installed dependencies"
 
-RELEASE=$(curl -fsSL https://teamspeak.com/en/downloads/#server | sed -n 's/.*teamspeak3-server_linux_amd64-\([0-9]\+\.[0-9]\+\.[0-9]\+\).*/\1/p;q')
+RELEASE=$(curl -fsSL https://teamspeak.com/en/downloads/#server | sed -n '/teamspeak3-server_linux_amd64-/ { s/.*teamspeak3-server_linux_amd64-\([0-9]\+\.[0-9]\+\.[0-9]\+\).*/\1/p; q }')
 
 msg_info "Installing Teamspeak Server v${RELEASE}"
 mkdir -p /opt/teamspeak-server
