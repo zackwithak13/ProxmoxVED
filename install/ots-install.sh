bc3abac12f2c059a9eec01c01e78c067f0c2d05a
Bas van den Berg <74251551+bvdberg01@users.noreply.github.com>
2025-07-28 21:26:35 +0200

    Update ots-install.sh

--- install/ots-install.sh
+++ install/ots-install.sh
@@ -51,12 +51,12 @@ server {
   location / {
     add_header X-Robots-Tag noindex;
 
-    proxy_set_header Upgrade $http_upgrade;
+    proxy_set_header Upgrade \$http_upgrade;
     proxy_set_header Connection "Upgrade";
-    proxy_set_header Host $host;
-    proxy_set_header X-Real-IP $remote_addr;
-    proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-    proxy_set_header X-Forwarded-Proto $scheme;
+    proxy_set_header Host \$host;
+    proxy_set_header X-Real-IP \$remote_addr;
+    proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+    proxy_set_header X-Forwarded-Proto \$scheme;
 
     proxy_pass http://127.0.0.1:3000/;
   }
