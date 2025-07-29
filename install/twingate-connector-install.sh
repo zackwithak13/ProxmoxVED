c4bedd9d280f96d408f0e7bfc257ffab830763ec
Andrew Baumbach <andrewb@twingate.com>
2025-07-29 09:45:12 -0400

    chore: update author info

--- install/twingate-connector-install.sh
+++ install/twingate-connector-install.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 # Copyright (c) 2021-2025 community-scripts ORG
-# Author: [Andrew Baumbach]
+# Author: twingate-andrewb
 # License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
-# Source: [https://raw.githubusercontent.com/twingate-andrewb/ProxmoxVE] // TODO: change this
+# Source: https://www.twingate.com/docs/
 
 # Import Functions und Setup
 source /dev/stdin <<<"$FUNCTIONS_FILE_PATH"
