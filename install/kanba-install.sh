5156ad9126719c0d90d6ae481446be8242ba2615
CanbiZ <47820557+MickLesk@users.noreply.github.com>
2025-07-29 13:12:25 +0200

    Update kanba-install.sh

--- install/kanba-install.sh
+++ install/kanba-install.sh
@@ -13,7 +13,7 @@ setting_up_container
 network_check
 update_os
 
-NODE_VERSION="22" NODE_MODULE="npx" setup_nodejs
+NODE_VERSION="22" setup_nodejs
 fetch_and_deploy_gh_release "kanba" "Kanba-co/kanba" "tarball" "latest" "/opt/kanba"
 POSTGRES_VERSION="16" setup_postgresql
 
