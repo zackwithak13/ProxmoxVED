c99bb7ca6c57a29963b4a547deb66a978519a04d
Bas van den Berg <74251551+bvdberg01@users.noreply.github.com>
2025-07-31 20:40:05 +0200

    Update ots-install.sh

--- install/ots-install.sh
+++ install/ots-install.sh
@@ -44,7 +44,7 @@ server {
     listen 80;
     listen [::]:80;
     server_name ots;
-    return 301 https://$host$request_uri;
+    return 301 https://\$host\$request_uri;
 }
 server {
   listen 443 ssl;
