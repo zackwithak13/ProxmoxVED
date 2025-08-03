0b0656795d7c58221993f2919a121d3a14e3b7cf
CanbiZ <47820557+MickLesk@users.noreply.github.com>
2025-08-03 20:10:59 +0200

    fixes

--- install/proxmox-backup-server-install.sh
+++ install/proxmox-backup-server-install.sh
@@ -16,17 +16,14 @@ update_os
 read -rp "${TAB3}Do you want to use the Enterprise repository (requires valid subscription key)? [y/N]: " USE_ENTERPRISE_REPO
 
 msg_info "Installing Proxmox Backup Server"
+curl -fsSL https://enterprise.proxmox.com/debian/proxmox-release-bookworm.gpg |
+    gpg --dearmor -o /etc/apt/trusted.gpg.d/proxmox-release-bookworm.gpg
 if [[ "$USE_ENTERPRISE_REPO" =~ ^([yY].*)$ ]]; then
     echo "deb https://enterprise.proxmox.com/debian/pbs bookworm pbs-enterprise" >/etc/apt/sources.list.d/pbs-enterprise.list
-    sed -i '/pbs-no-subscription/s/^/#/' /etc/apt/sources.list
-    msg_custom "Enterprise repository enabled. Make sure your subscription key is installed."
+    msg_ok "Enterprise repository enabled. Make sure your subscription key is installed."
 else
-    if ! grep -q "pbs-no-subscription" /etc/apt/sources.list; then
-        echo "deb http://download.proxmox.com/debian/pbs bookworm pbs-no-subscription" >>/etc/apt/sources.list
-    else
-        sed -i '/pbs-no-subscription/s/^#//' /etc/apt/sources.list
-    fi
-    rm -f /etc/apt/sources.list.d/pbs-enterprise.list
+    echo "deb http://download.proxmox.com/debian/pbs bookworm pbs-no-subscription" >>/etc/apt/sources.list
+    msg_ok "No-subscription repository enabled."
 fi
 
 $STD apt-get update
