bca8a10f7e447e666448497badd3ef2fb9758d2b
CanbiZ <47820557+MickLesk@users.noreply.github.com>
2025-08-04 09:38:06 +0200

    changes

--- install/swizzin-install.sh
+++ install/swizzin-install.sh
@@ -13,14 +13,20 @@ setting_up_container
 network_check
 update_os
 
-msg_info "Installing Dependencies"
-$STD apt-get install -y curl wget gnupg lsb-release
-msg_ok "Dependencies installed"
-echo -e "Launching upstream Swizzin installer..."
+msg_custom "!" "$YELLOW" "WARNING: This script will run an external installer from a third-party source (swizzin.io)."
+msg_custom "!" "$YELLOW" "You are about to execute code that is NOT maintained by our repository."
+echo
+read -r -p "Do you want to continue? [y/N]: " CONFIRM
+if [[ ! "$CONFIRM" =~ ^([yY][eE][sS]|[yY])$ ]]; then
+  msg_error "Aborted by user. No changes have been made."
+  exit 1
+fi
 bash <(curl -sL s5n.sh)
+
+motd_ssh
+customize
+
 msg_info "Cleaning up"
 $STD apt-get -y autoremove
 $STD apt-get -y autoclean
 msg_ok "Cleaned"
-
-msg_ok "Swizzin base installation complete.  Re‑login and run 'sudo box' to add apps."
