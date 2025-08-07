a0ea3a2f176a5671e9f247fc3263218082585582
Tobias <96661824+CrazyWolf13@users.noreply.github.com>
2025-08-07 13:44:35 +0200

    Update tracktor.sh

--- ct/tracktor.sh
+++ ct/tracktor.sh
@@ -7,9 +7,9 @@ source <(curl -s https://raw.githubusercontent.com/community-scripts/ProxmoxVED/
 
 APP="tracktor"
 var_tags="${var_tags:-car;monitoring}"
-var_cpu="${var_cpu:-1}"
-var_ram="${var_ram:-1024}"
-var_disk="${var_disk:-5}"
+var_cpu="${var_cpu:-2}"
+var_ram="${var_ram:-4096}"
+var_disk="${var_disk:-6}"
 var_os="${var_os:-debian}"
 var_version="${var_version:-12}"
 var_unprivileged="${var_unprivileged:-1}"
