709ed8917e08f38e0d3c19c66bc6e7aaaef2b6a9
CanbiZ <47820557+MickLesk@users.noreply.github.com>
2025-08-18 08:10:53 +0200

    Update twingate-connector.sh

--- ct/twingate-connector.sh
+++ ct/twingate-connector.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-source <(curl -fsSL https://raw.githubusercontent.com/community-scripts/ProxmoxVED/misc/build.func)
+source <(curl -fsSL https://raw.githubusercontent.com/community-scripts/ProxmoxVED/main/misc/build.func)
 # Copyright (c) 2021-2025 community-scripts ORG
 # Author: twingate-andrewb
 # License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
