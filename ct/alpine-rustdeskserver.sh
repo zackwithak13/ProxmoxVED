05347048c538b593588a1b3f7a543d6662684a6d
tremor021 <arezina.slavisa@gmail.com>
2025-08-20 13:59:07 +0200

    Add Alpine RustDesk Server script

---
+++ ct/alpine-rustdeskserver.sh
@@ -0,0 +1,62 @@
+#!/usr/bin/env bash
+source <(curl -fsSL https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/misc/build.func)
+# Copyright (c) 2021-2025 community-scripts ORG
+# Author: Slaviša Arežina (tremor021)
+# License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
+# Source: https://github.com/rustdesk/rustdesk-server
+
+APP="Alpine-RustDesk Server"
+var_tags="${var_tags:-alpine;monitoring}"
+var_cpu="${var_cpu:-1}"
+var_ram="${var_ram:-512}"
+var_disk="${var_disk:-3}"
+var_os="${var_os:-alpine}"
+var_version="${var_version:-3.22}"
+var_unprivileged="${var_unprivileged:-1}"
+
+header_info "$APP"
+variables
+color
+catch_errors
+
+function update_script() {
+  header_info
+
+  if [[ ! -d /opt/rustdesk-server ]]; then
+    msg_error "No ${APP} Installation Found!"
+    exit 1
+  fi
+  RELEASE=$(curl -s https://api.github.com/repos/TwiN/RustDesk Server/releases/latest | grep "tag_name" | awk '{print substr($2, 3, length($2)-4) }')
+  if [ "${RELEASE}" != "$(cat /opt/RustDesk Server_version.txt)" ] || [ ! -f /opt/RustDesk Server_version.txt ]; then
+    msg_info "Updating ${APP} LXC"
+    $STD apk -U upgrade
+    $STD service RustDesk Server stop
+    mv /opt/RustDesk Server/config/config.yaml /opt
+    rm -rf /opt/RustDesk Server/*
+    temp_file=$(mktemp)
+    curl -fsSL "https://github.com/TwiN/RustDesk Server/archive/refs/tags/v${RELEASE}.tar.gz" -o "$temp_file"
+    tar zxf "$temp_file" --strip-components=1 -C /opt/RustDesk Server
+    cd /opt/RustDesk Server
+    $STD go mod tidy
+    CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o RustDesk Server .
+    setcap CAP_NET_RAW+ep RustDesk Server
+    mv /opt/config.yaml config
+    rm -f "$temp_file"
+    echo "${RELEASE}" >/opt/RustDesk Server_version.txt
+    $STD service RustDesk Server start
+    msg_ok "Updated Successfully"
+  else
+    msg_ok "No update required. ${APP} is already at ${RELEASE}"
+  fi
+
+  exit 0
+}
+
+start
+build_container
+description
+
+msg_ok "Completed Successfully!\n"
+echo -e "${CREATING}${GN}${APP} setup has been successfully initialized!${CL}"
+echo -e "${INFO}${YW} Access it using the following IP:${CL}"
+echo -e "${TAB}${GATEWAY}${BGN}http://${IP}:8080${CL}"
---
+++ install/alpine-rustdeskserver-install.sh
@@ -0,0 +1,111 @@
+#!/usr/bin/env bash
+
+# Copyright (c) 2021-2025 community-scripts ORG
+# Author: Slaviša Arežina (tremor021)
+# License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
+# Source: https://github.com/rustdesk/rustdesk-server
+
+source /dev/stdin <<<"$FUNCTIONS_FILE_PATH"
+color
+verb_ip6
+catch_errors
+setting_up_container
+network_check
+update_os
+
+RELEASE=$(curl -s https://api.github.com/repos/rustdesk/rustdesk-server/releases/latest | grep "tag_name" | awk '{print substr($2, 2, length($2)-3) }')
+msg_info "Installing RustDesk Server v${RELEASE}"
+temp_file1=$(mktemp)
+curl -fsSL "https://github.com/rustdesk/rustdesk-server/releases/download/${RELEASE}/rustdesk-server-linux-amd64.zip" -o "$temp_file"
+unzip "$temp_file1"
+mv amd64 /opt/rustdesk-server
+mkdir -p /root/.config/rustdesk
+cd /opt/rustdesk-server
+$STD ./rustdesk-utils genkeypair | tee \
+  >(grep "Public Key" | awk '{print $3}' > /root/.config/rustdesk/id_ed25519.pub) \
+  >(grep "Secret Key" | awk '{print $3}' > /root/.config/rustdesk/id_ed25519)
+chmod 600 /root/.config/rustdesk/id_ed25519
+chmod 644 /root/.config/rustdesk/id_ed25519.pub
+echo "${RELEASE}" >~/.rustdesk-server
+msg_ok "Installed RustDesk Server v${RELEASE}"
+
+APIRELEASE=$(curl -s https://api.github.com/repos/lejianwen/rustdesk-api/releases/latest | grep "tag_name" | awk '{print substr($2, 3, length($2)-4) }')
+msg_info "Installing RustDesk API v${APIRELEASE}"
+temp_file2=$(mktemp)
+curl -fsSL "https://github.com/lejianwen/rustdesk-api/releases/download/v${APIRELEASE}/linux-amd64.tar.gz" -o $temp_file2
+tar zxvf "$temp_file2"
+mv release /opt/rustdesk-api
+msg_ok "Installed RustDesk API v${APIRELEASE}"
+
+msg_info "Enabling RustDesk Server Services"
+cat <<EOF >/etc/init.d/rustdesk-server-hbbs
+#!/sbin/openrc-run
+description="RustDesk HBBS Service"
+directory="/opt/rustdesk-server"
+command="/opt/rustdesk-server/hbbs"
+command_args=""
+command_background="true"
+command_user="root"
+pidfile="/var/run/rustdesk-server-hbbs.pid"
+output_log="/var/log/rustdesk-hbbs.log"
+error_log="/var/log/rustdesk-hbbs.err"
+
+depend() {
+    use net
+}
+EOF
+
+cat <<EOF >/etc/init.d/rustdesk-server-hbbr
+#!/sbin/openrc-run
+description="RustDesk HBBR Service"
+directory="/opt/rustdesk-server"
+command="/opt/rustdesk-server/hbbr"
+command_args=""
+command_background="true"
+command_user="root"
+pidfile="/var/run/rustdesk-server-hbbr.pid"
+output_log="/var/log/rustdesk-hbbr.log"
+error_log="/var/log/rustdesk-hbbr.err"
+
+depend() {
+    use net
+}
+EOF
+
+cat <<EOF >/etc/init.d/rustdesk-api
+#!/sbin/openrc-run
+description="RustDesk API Service"
+directory="/opt/rustdesk-api"
+command="/opt/rustdesk-api/apimain"
+command_args=""
+command_background="true"
+command_user="root"
+pidfile="/var/run/rustdesk-server-hbbr.pid"
+output_log="/var/log/rustdesk-api.log"
+error_log="/var/log/rustdesk-api.err"
+
+depend() {
+    use net
+}
+EOF
+chmod +x /etc/init.d/rustdesk-server-hbbs
+chmod +x /etc/init.d/rustdesk-server-hbbr
+chmod +x /etc/init.d/rustdesk-api
+$STD rc-update add rustdesk-server-hbbs default
+$STD rc-update add rustdesk-server-hbbr default
+$STD rc-update add rustdesk-api default
+msg_ok "Enabled RustDesk Server Services"
+
+msg_info "Starting RustDesk Server"
+$STD service rustdesk-server-hbbs start
+$STD service rustdesk-server-hbbr start
+$STD service rustdesk-api start
+msg_ok "Started RustDesk Server"
+
+motd_ssh
+customize
+
+msg_info "Cleaning up"
+rm -f "$temp_file1" "$temp_file2"
+$STD apk cache clean
+msg_ok "Cleaned"
