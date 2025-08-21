87556664137c1dfa790b0e13472a5b880bd3664d
CanbiZ <47820557+MickLesk@users.noreply.github.com>
2025-08-21 13:23:17 +0200

    Update healthchecks.sh

--- ct/healthchecks.sh
+++ ct/healthchecks.sh
@@ -20,15 +20,15 @@ color
 catch_errors
 
 function update_script() {
-    header_info
-    check_container_storage
-    check_container_resources
-    if [[ ! -f /etc/systemd/system/healthchecks.service ]]; then
-        msg_error "No ${APP} Installation Found!"
-        exit
-    fi
-    msg_error "No Update."
+  header_info
+  check_container_storage
+  check_container_resources
+  if [[ ! -f /etc/systemd/system/healthchecks.service ]]; then
+    msg_error "No ${APP} Installation Found!"
     exit
+  fi
+  msg_error "No Update."
+  exit
 }
 
 start
@@ -38,4 +38,4 @@ description
 msg_ok "Completed Successfully!\n"
 echo -e "${CREATING}${GN}${APP} setup has been successfully initialized!${CL}"
 echo -e "${INFO}${YW} Access it using the following URL:${CL}"
-echo -e "${TAB}${GATEWAY}${BGN}http://${IP}:8000${CL}"
+echo -e "${TAB}${GATEWAY}${BGN}https://${IP}${CL}"
