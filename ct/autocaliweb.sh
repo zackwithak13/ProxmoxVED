c97be769aae8b9c6bf740bcd456ab578184b586e
vhsdream <punk.sand7393@fastmail.com>
2025-08-27 09:10:17 -0400

    Autocaliweb: add env file to temp backup

--- ct/autocaliweb.sh
+++ ct/autocaliweb.sh
@@ -38,7 +38,7 @@ function update_script() {
 
     INSTALL_DIR="/opt/autocaliweb"
     VIRTUAL_ENV="${INSTALL_DIR}/venv"
-    $STD tar -cf ~/autocaliweb_bkp.tar "$INSTALL_DIR"/{metadata_change_logs,dirs.json,scripts/ingest_watcher.sh,scripts/auto_zipper_wrapper.sh,scripts/metadata_change_detector_wrapper.sh}
+    $STD tar -cf ~/autocaliweb_bkp.tar "$INSTALL_DIR"/{metadata_change_logs,dirs.json,.env,scripts/ingest_watcher.sh,scripts/auto_zipper_wrapper.sh,scripts/metadata_change_detector_wrapper.sh}
     fetch_and_deploy_gh_release "autocaliweb" "gelbphoenix/autocaliweb" "tarball" "latest" "/opt/autocaliweb"
     msg_info "Updating ${APP}"
     cd "$INSTALL_DIR"
