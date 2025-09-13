b1797e079389ad5368f341bcca94637d053ac950
Tobias <96661824+CrazyWolf13@users.noreply.github.com>
2025-09-13 12:54:18 +0200

    Fix installation command for Proxmox Datacenter Manager

--- install/proxmox-datacenter-manager-install.sh
+++ install/proxmox-datacenter-manager-install.sh
@@ -17,7 +17,8 @@ msg_info "Installing Proxmox Datacenter Manager"
 curl -fsSL https://enterprise.proxmox.com/debian/proxmox-archive-keyring-trixie.gpg -o /usr/share/keyrings/proxmox-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/proxmox-archive-keyring.gpg] http://download.proxmox.com/debian/pdm bookworm pdm-test  " >/etc/apt/sources.list.d/pdm-test.list
 $STD apt-get update
-$STD DEBIAN_FRONTEND=noninteractive apt-get -o Dpkg::Options::="--force-confdef" \
+DEBIAN_FRONTEND=noninteractive
+$STD apt-get -o Dpkg::Options::="--force-confdef" \
         -o Dpkg::Options::="--force-confold" \
         install -y proxmox-datacenter-manager \
         proxmox-datacenter-manager-ui
