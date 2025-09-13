3311221d5e20059b6dcb47b3bd6773984ad4866e
Tobias <96661824+CrazyWolf13@users.noreply.github.com>
2025-09-13 13:23:01 +0200

    Fix source formats and update Proxmox keyrings
    Updated outdated source formats for Proxmox and removed old keyrings.

--- ct/proxmox-datacenter-manager.sh
+++ ct/proxmox-datacenter-manager.sh
@@ -29,10 +29,12 @@ function update_script() {
     fi
 
     if grep -q 'Debian GNU/Linux 12' /etc/os-release && [ -f /etc/apt/sources.list.d/proxmox-release-bookworm.list ] && [ -f /etc/apt/sources.list.d/pdm-test.list ]; then
+        msg_info "Updating outdated outdated source formats"
         echo "deb [signed-by=/usr/share/keyrings/proxmox-archive-keyring.gpg] http://download.proxmox.com/debian/pdm bookworm pdm-test" > /etc/apt/sources.list.d/pdm-test.list
         curl -fsSL https://enterprise.proxmox.com/debian/proxmox-archive-keyring-trixie.gpg -o /usr/share/keyrings/proxmox-archive-keyring.gpg
         rm -f /etc/apt/keyrings/proxmox-release-bookworm.gpg /etc/apt/sources.list.d/proxmox-release-bookworm.list
         $STD apt update
+        msg_ok "Updated old sources"
     fi
 
     msg_info "Updating $APP LXC"
