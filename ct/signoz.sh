fbb3cf46c1dc74f7e9a05fbe7627446ca3968381
tremor021 <arezina.slavisa@gmail.com>
2025-09-14 14:51:59 +0200

    Update SigNoz

--- ct/signoz.sh
+++ ct/signoz.sh
@@ -31,15 +31,21 @@ function update_script() {
   if check_for_gh_release "signoz" "SigNoz/signoz"; then
     msg_info "Stopping Services"
     systemctl stop signoz
+    systemctl stop signoz-otel-collector
     msg_ok "Stopped Services"
 
     fetch_and_deploy_gh_release "signoz" "SigNoz/signoz" "prebuild" "latest" "/opt/signoz" "signoz-community_linux_amd64.tar.gz"
+    fetch_and_deploy_gh_release "signoz-otel-collector" "SigNoz/signoz-otel-collector" "prebuild" "latest" "/opt/signoz-otel-collector" "signoz-otel-collector_linux_amd64.tar.gz"
+    fetch_and_deploy_gh_release "signoz-schema-migrator" "SigNoz/signoz-otel-collector" "prebuild" "latest" "/opt/signoz-schema-migrator" "signoz-schema-migrator_linux_amd64.tar.gz"
 
     msg_info "Updating ${APP}"
-
+    cd /opt/signoz-schema-migrator/bin
+    $STD ./signoz-schema-migrator sync --dsn="tcp://localhost:9000?password=" --replication=true  --up=
+    $STD ./signoz-schema-migrator async --dsn="tcp://localhost:9000?password=" --replication=true  --up=
     msg_ok "Updated $APP"
 
     msg_info "Starting Services"
+    systemctl start signoz-otel-collector
     systemctl start signoz
     msg_ok "Started Services"
 
