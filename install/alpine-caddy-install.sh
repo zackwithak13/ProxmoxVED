bd1dbc64b1de552a145879825fb5b257533202a5
cobaltgit <cobaltsecuremail312@proton.me>
2025-09-15 13:34:12 +0100

    alpine-caddy: use apk for xcaddy

--- install/alpine-caddy-install.sh
+++ install/alpine-caddy-install.sh
@@ -19,14 +19,8 @@ msg_ok "Installed Caddy"
 
 read -r -p "${TAB3}Would you like to install xCaddy Addon? <y/N> " prompt
 if [[ "${prompt,,}" =~ ^(y|yes)$ ]]; then
-  setup_go
   msg_info "Setup xCaddy"
-  $STD apk add --no-cache git
-  cd /opt
-  RELEASE=$(curl -fsSL https://api.github.com/repos/caddyserver/xcaddy/releases/latest | grep "tag_name" | awk '{print substr($2, 2, length($2)-3) }')
-  curl -fsSL "https://github.com/caddyserver/xcaddy/releases/download/${RELEASE}/xcaddy_${RELEASE:1}_linux_amd64.tar.gz" -o "xcaddy_${RELEASE:1}_linux_amd64.tar.gz"
-  $STD tar xzf xcaddy_"${RELEASE:1}"_linux_amd64.tar.gz -C /usr/local/bin
-  rm -rf /opt/xcaddy*
+  $STD apk add --no-cache xcaddy
   $STD xcaddy build
   msg_ok "Setup xCaddy"
 fi
