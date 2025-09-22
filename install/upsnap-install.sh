9d35c3124321ee8df028a6916d20cb2ea0d26ef2
CanbiZ <47820557+MickLesk@users.noreply.github.com>
2025-09-22 19:51:00 +0200

    deb13

--- install/upsnap-install.sh
+++ install/upsnap-install.sh
@@ -14,7 +14,7 @@ network_check
 update_os
 
 msg_info "Installing Dependencies"
-$STD apt-get install -y \
+$STD apt install -y \
   nmap \
   samba \
   samba-common-bin \
@@ -50,6 +50,7 @@ motd_ssh
 customize
 
 msg_info "Cleaning up"
-$STD apt-get -y autoremove
-$STD apt-get -y autoclean
+$STD apt -y autoremove
+$STD apt -y autoclean
+$STD apt -y clean
 msg_ok "Cleaned"
