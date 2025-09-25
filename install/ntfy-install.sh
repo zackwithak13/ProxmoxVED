11d8da538bf488f06af1f0100323df8c89e60d22
Tobias <96661824+CrazyWolf13@users.noreply.github.com>
2025-09-25 10:23:49 +0200

    Update ntfy installation script with new repository

--- install/ntfy-install.sh
+++ install/ntfy-install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Copyright (c) 2021-2025 tteck
-# Author: tteck (tteckster)
+# Author: tteck (tteckster) | Co-Author: CrazyWolf13
 # License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
 # Source: https://ntfy.sh/
 
@@ -13,17 +13,20 @@ setting_up_container
 network_check
 update_os
 
-msg_info "Installing Dependencies"
-$STD apt-get install -y \
-  apt-transport-https
-msg_ok "Installed Dependencies"
-
 msg_info "Installing ntfy"
 mkdir -p /etc/apt/keyrings
-curl -fsSL https://archive.heckel.io/apt/pubkey.txt | gpg --dearmor -o /etc/apt/keyrings/archive.heckel.io.gpg
-echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/archive.heckel.io.gpg] https://archive.heckel.io/apt debian main" >/etc/apt/sources.list.d/archive.heckel.io.list
-$STD apt-get update
-$STD apt-get install -y ntfy
+sudo curl -fsSL -o /etc/apt/keyrings/ntfy.gpg https://archive.ntfy.sh/apt/keyring.gpg
+
+cat <<'EOF' >/etc/apt/sources.list.d/ntfy.sources 
+Types: deb
+URIs: https://archive.ntfy.sh/apt/
+Suites: stable
+Components: main
+Signed-By: /etc/apt/keyrings/ntfy.gpg
+EOF
+
+$STD apt update
+$STD apt install -y ntfy
 systemctl enable -q --now ntfy
 msg_ok "Installed ntfy"
 
@@ -31,6 +34,6 @@ motd_ssh
 customize
 
 msg_info "Cleaning up"
-$STD apt-get -y autoremove
-$STD apt-get -y autoclean
+$STD apt -y autoremove
+$STD apt -y autoclean
 msg_ok "Cleaned"
