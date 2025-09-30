e33f32b56ef13190ee1996c08a104aebde94e0c6
Slaviša Arežina <58952836+tremor021@users.noreply.github.com>
2025-10-01 00:41:26 +0200

    Update install/sonarqube-install.sh

--- install/sonarqube-install.sh
+++ install/sonarqube-install.sh
@@ -20,7 +20,6 @@ msg_info "Installing Postgresql"
 DB_NAME="sonarqube"
 DB_USER="sonarqube"
 DB_PASS=$(openssl rand -base64 18 | tr -dc 'a-zA-Z0-9' | cut -c1-13)
-systemctl start postgresql
 $STD sudo -u postgres psql -c "CREATE USER $DB_USER WITH PASSWORD '$DB_PASS';"
 $STD sudo -u postgres psql -c "CREATE DATABASE $DB_NAME OWNER $DB_USER;"
 {
