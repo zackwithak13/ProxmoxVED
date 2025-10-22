dad22d88fdd69c0fae14d07f102bfa3077bad16e
vhsdream <punk.sand7393@fastmail.com>
2025-10-22 16:13:35 -0400

    Add files for PatchMon

---
+++ ct/patchmon.sh
@@ -0,0 +1,79 @@
+#!/usr/bin/env bash
+source <(curl -fsSL https://raw.githubusercontent.com/community-scripts/ProxmoxVED/main/misc/build.func)
+# Copyright (c) 2021-2025 community-scripts ORG
+# Author: vhsdream
+# License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
+# Source: https://github.com/PatchMon/PatchMon
+
+APP="PatchMon"
+APP_NAME=${APP,,}
+var_tags="${var_tags:-monitoring}"
+var_cpu="${var_cpu:-2}"
+var_ram="${var_ram:-2048}"
+var_disk="${var_disk:-4}"
+var_os="${var_os:-debian}"
+var_version="${var_version:-13}"
+var_unprivileged="${var_unprivileged:-1}"
+
+header_info "$APP"
+variables
+color
+catch_errors
+
+function update_script() {
+  header_info
+  check_container_storage
+  check_container_resources
+
+  if [[ ! -d "/opt/patchmon" ]]; then
+    msg_error "No ${APP} Installation Found!"
+    exit
+  fi
+
+  if check_for_gh_release "PatchMon" "PatchMon/PatchMon"; then
+
+    msg_info "Stopping $APP"
+    systemctl stop patchmon-server
+    msg_ok "Stopped $APP"
+
+    msg_info "Creating Backup"
+    cp /opt/patchmon/backend/.env /opt/backend.env
+    cp /opt/patchmon/frontend/.env /opt/frontend.env
+    msg_ok "Backup Created"
+
+    rm -rf /opt/patchmon
+    fetch_and_deploy_gh_release "PatchMon" "PatchMon/PatchMon" "tarball" "latest" "/opt/patchmon"
+
+    msg_info "Updating ${APP}"
+    cd /opt/patchmon
+    export NODE_ENV=production
+    export NPM_CONFIG_CACHE=/opt/patchmon/.npm
+    export NPM_CONFIG_PREFIX=/opt/patchmon/.npm-global
+    export NPM_CONFIG_TMP=/opt/patchmon/.npm/tmp
+    $STD npm install --omit=dev --no-audit --no-fund --no-save --ignore-scripts
+    cd /opt/patchmon/backend
+    $STD npm install --omit=dev --no-audit --no-fund --no-save --ignore-scripts
+    cd /opt/patchmon/frontend
+    $STD npm install --no-audit --no-fund --no-save --ignore-scripts
+    $STD npm run build
+    cd /opt/patchmon/backend
+    $STD npx prisma migrate deploy
+    mv /opt/backend.env /opt/patchmon/backend/.env
+    mv /opt/frontend.env /opt/patchmon/frontend/.env
+    msg_ok "Updated ${APP}"
+
+    msg_info "Starting $APP"
+    systemctl start patchmon-server
+    msg_ok "Started $APP"
+  fi
+  exit
+}
+
+start
+build_container
+description
+
+msg_ok "Completed Successfully!\n"
+echo -e "${CREATING}${GN}${APP} setup has been successfully initialized!${CL}"
+echo -e "${INFO}${YW} Access it using the following URL:${CL}"
+echo -e "${TAB}${GATEWAY}${BGN}http://${IP}${CL}"
