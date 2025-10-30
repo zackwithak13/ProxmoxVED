82e96b7dba483f429471b5cd6ce427f23c27424d
tremor021 <arezina.slavisa@gmail.com>
2025-10-30 11:27:45 +0100

    Update infisical

--- install/infisical-install.sh
+++ install/infisical-install.sh
@@ -33,8 +33,8 @@ msg_ok "Setup Infisical repository"
 PG_VERSION="17" setup_postgresql
 
 msg_info "Setting up PostgreSQL"
-DB_NAME="infiscal_db"
-DB_USER="infiscal"
+DB_NAME="infisical_db"
+DB_USER="infisical"
 DB_PASS="$(openssl rand -base64 18 | tr -dc 'a-zA-Z0-9' | cut -c1-13)"
 $STD sudo -u postgres psql -c "CREATE ROLE $DB_USER WITH LOGIN PASSWORD '$DB_PASS';"
 $STD sudo -u postgres psql -c "CREATE DATABASE $DB_NAME WITH OWNER $DB_USER ENCODING 'UTF8' TEMPLATE template0;"
@@ -46,7 +46,7 @@ $STD sudo -u postgres psql -c "ALTER ROLE $DB_USER SET timezone TO 'UTC';"
   echo "Database Name: $DB_NAME"
   echo "Database User: $DB_USER"
   echo "Database Password: $DB_PASS"
-} >>~/infiscal.creds
+} >>~/infisical.creds
 msg_ok "Setup PostgreSQL"
 
 msg_info "Setting up Infisical"
