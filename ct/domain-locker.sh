8fc5443ed54b8e1d301ee3d5b1ef15af1cce57b4
Tobias <96661824+CrazyWolf13@users.noreply.github.com>
2025-11-10 22:01:35 +0100

    Update domain-locker.sh

--- ct/domain-locker.sh
+++ ct/domain-locker.sh
@@ -8,7 +8,7 @@ source <(curl -fsSL https://raw.githubusercontent.com/community-scripts/ProxmoxV
 APP="Domain-Locker"
 var_tags="${var_tags:-Monitoring}"
 var_cpu="${var_cpu:-1}"
-var_ram="${var_ram:-1024}"
+var_ram="${var_ram:-2048}"
 var_disk="${var_disk:-8}"
 var_os="${var_os:-debian}"
 var_version="${var_version:-13}"
