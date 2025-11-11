5c80aea6420f52357f8e9e7be7eee88ba61ad891
Tobias <96661824+CrazyWolf13@users.noreply.github.com>
2025-11-11 10:58:06 +0100

    Update default CPU and RAM settings
    Increase default CPU and RAM values for Domain-Locker.

--- ct/domain-locker.sh
+++ ct/domain-locker.sh
@@ -7,8 +7,8 @@ source <(curl -fsSL https://raw.githubusercontent.com/community-scripts/ProxmoxV
 
 APP="Domain-Locker"
 var_tags="${var_tags:-Monitoring}"
-var_cpu="${var_cpu:-2}"
-var_ram="${var_ram:-4096}"
+var_cpu="${var_cpu:-4}"
+var_ram="${var_ram:-8192}"
 var_disk="${var_disk:-8}"
 var_os="${var_os:-debian}"
 var_version="${var_version:-13}"
