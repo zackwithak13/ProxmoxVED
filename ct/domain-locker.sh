f0d295ffdc58e26501c68c75276dee2f6ed39950
Tobias <96661824+CrazyWolf13@users.noreply.github.com>
2025-11-14 13:43:31 +0100

    Increase default RAM from 8192 to 10240

--- ct/domain-locker.sh
+++ ct/domain-locker.sh
@@ -8,7 +8,7 @@ source <(curl -fsSL https://raw.githubusercontent.com/community-scripts/ProxmoxV
 APP="Domain-Locker"
 var_tags="${var_tags:-Monitoring}"
 var_cpu="${var_cpu:-4}"
-var_ram="${var_ram:-8192}"
+var_ram="${var_ram:-10240}"
 var_disk="${var_disk:-8}"
 var_os="${var_os:-debian}"
 var_version="${var_version:-13}"
