5ce22bbbe67ac31360d7856324703a03ff016ea2
Tobias <96661824+CrazyWolf13@users.noreply.github.com>
2025-11-14 13:41:32 +0100

    Update GitHub repository for domain-locker installation

--- install/domain-locker-install.sh
+++ install/domain-locker-install.sh
@@ -17,7 +17,7 @@ PG_VERSION="17" setup_postgresql
 PG_DB_NAME="domainlocker" PG_DB_USER="domainlocker" setup_postgresql_db
 NODE_VERSION="22" setup_nodejs
 
-fetch_and_deploy_gh_release "domain-locker" "Lissy93/domain-locker"
+fetch_and_deploy_gh_release "domain-locker" "CrazyWolf13/domain-locker"
 
 msg_info "Installing Modules (patience)"
 cd /opt/domain-locker
