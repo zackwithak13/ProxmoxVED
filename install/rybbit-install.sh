186651349d37689239d04b347e8f8dbd9c5d8052
CanbiZ <47820557+MickLesk@users.noreply.github.com>
2025-11-17 09:09:23 +0100

    Update rybbit-install.sh

--- install/rybbit-install.sh
+++ install/rybbit-install.sh
@@ -15,37 +15,19 @@ update_os
 
 msg_info "Installing Dependencies"
 $STD apt install -y \
-    caddy \
-    apt-transport-https \
-    ca-certificates
+  caddy \
+  apt-transport-https \
+  ca-certificates
 msg_ok "Installed Dependencies"
 
 setup_clickhouse
 PG_VERSION=17 setup_postgresql
 NODE_VERSION="20" NODE_MODULE="next" setup_nodejs
-
-#sed -i 's|<default_profile>default</default_profile>|<default_profile>read_only</default_profile>|' /etc/clickhouse-server/users.xml
-#sed -i 's|<default_password></default_password>|<default_password>DISABLED</default_password>|' /etc/clickhouse-server/users.xml
-
-msg_info "Setting up PostgreSQL Database"
-DB_NAME=rybbit_db
-DB_USER=rybbit
-DB_PASS="$(openssl rand -base64 18 | cut -c1-13)"
-$STD sudo -u postgres psql -c "CREATE ROLE $DB_USER WITH LOGIN PASSWORD '$DB_PASS';"
-$STD sudo -u postgres psql -c "CREATE DATABASE $DB_NAME WITH OWNER $DB_USER ENCODING 'UTF8' TEMPLATE template0;"
-$STD sudo -u postgres psql -c "ALTER ROLE $DB_USER SET client_encoding TO 'utf8';"
-$STD sudo -u postgres psql -c "ALTER ROLE $DB_USER SET default_transaction_isolation TO 'read committed';"
-$STD sudo -u postgres psql -c "ALTER ROLE $DB_USER SET timezone TO 'UTC'"
-{
-    echo "Rybbit-Credentials"
-    echo "Rybbit Database User: $DB_USER"
-    echo "Rybbit Database Password: $DB_PASS"
-    echo "Rybbit Database Name: $DB_NAME"
-} >>~/rybbit.creds
-msg_ok "Set up PostgreSQL Database"
+PG_DB_NAME="rybbit_db" PG_DB_USER="rybbit" setup_postgresql_db
 
 fetch_and_deploy_gh_release "rybbit" "rybbit-io/rybbit" "tarball" "latest" "/opt/rybbit"
 
+msg_info "Installing Rybbit"
 cd /opt/rybbit/shared
 npm install
 npm run build
@@ -59,9 +41,9 @@ npm ci --legacy-peer-deps
 npm run build
 
 mv /opt/rybbit/.env.example /opt/rybbit/.env
-sed -i "s|^POSTGRES_DB=.*|POSTGRES_DB=$DB_NAME|g" /opt/rybbit/.env
-sed -i "s|^POSTGRES_USER=.*|POSTGRES_USER=$DB_USER|g" /opt/rybbit/.env
-sed -i "s|^POSTGRES_PASSWORD=.*|POSTGRES_PASSWORD=$DB_PASS|g" /opt/rybbit/.env
+sed -i "s|^POSTGRES_DB=.*|POSTGRES_DB=$PG_DB_NAME|g" /opt/rybbit/.env
+sed -i "s|^POSTGRES_USER=.*|POSTGRES_USER=$PG_DB_USER|g" /opt/rybbit/.env
+sed -i "s|^POSTGRES_PASSWORD=.*|POSTGRES_PASSWORD=$PG_DB_PASS|g" /opt/rybbit/.env
 sed -i "s|^DOMAIN_NAME=.*|DOMAIN_NAME=localhost|g" /opt/rybbit/.env
 sed -i "s|^BASE_URL=.*|BASE_URL=\"http://localhost\"|g" /opt/rybbit/.env
 msg_ok "Rybbit Installed"
@@ -74,8 +56,4 @@ msg_ok "Caddy Setup"
 
 motd_ssh
 customize
-
-msg_info "Cleaning up"
-$STD apt-get -y autoremove
-$STD apt-get -y autoclean
-msg_ok "Cleaned"
+cleanup_lxc
