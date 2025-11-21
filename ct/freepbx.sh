fa335a11f54abf135dacd21976585103820aa95f
CanbiZ <47820557+MickLesk@users.noreply.github.com>
2025-11-21 09:40:43 +0100

    Remove Asterisk installation scripts
    Deleted ct/asterisk.sh and install/asterisk-install.sh, removing support for Asterisk container and installation. Updated ct/freepbx.sh to use the correct build.func source URL. This streamlines the codebase by dropping Asterisk-related scripts.

--- ct/freepbx.sh
+++ ct/freepbx.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-source <(curl -s https://raw.githubusercontent.com/vsc55/community-scripts-ProxmoxVED/refs/heads/freepbx/misc/build.func)
+source <(curl -s https://raw.githubusercontent.com/community-scripts/ProxmoxVED/refs/heads/freepbx/misc/build.func)
 # Copyright (c) 2021-2025 community-scripts ORG
 # Author: Arian Nasr (arian-nasr)
 # Updated by: Javier Pastor (vsc55)
