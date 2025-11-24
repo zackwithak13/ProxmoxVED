c66a062114550b284a497a7d3570db7c4848bfdf
SunFlowerOwl <drv.emeric@gmail.com>
2025-11-24 16:34:04 -0500

    Fix: Cable not redirect by reverse-proxy

--- install/manyfold-install.sh
+++ install/manyfold-install.sh
@@ -122,6 +122,19 @@ server {
     server_name manyfold;
     root /opt/manyfold/public;
 
+    location /cable {
+        proxy_pass http://127.0.0.1:5000;
+        proxy_set_header Host \$host;
+
+        proxy_http_version 1.1;
+        proxy_set_header Upgrade \$http_upgrade;
+        proxy_set_header Connection "Upgrade";
+
+        proxy_set_header X-Real-IP \$remote_addr;
+        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto \$scheme;
+    }
+
     location / {
         try_files \$uri/index.html \$uri @rails;
     }
