d2f36109fb4f75c8ffbfab65af631848ca8f4ac5
DragoQC <alexisgauthier2001@hotmail.com>
2025-11-25 16:59:24 -0500

    Fix discopanel source

--- ct/discopanel.sh
+++ ct/discopanel.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
-#source <(curl -fsSL https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/misc/build.func)
-source <(curl -fsSL https://raw.githubusercontent.com/DragoQC/proxmoxVE/main/misc/build.func)
+source <(curl -fsSL https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/misc/build.func)
 # Copyright (c) 2021-2025 community-scripts ORG
 # Author: DragoQC
 # License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
