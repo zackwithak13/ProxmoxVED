b7c10fa2f8d2766e7d8a132491180a402f5e09ed
Tobias <git@tobiasmeier.dev>
2025-12-08 19:44:11 +0100

    skip_tls

--- tools/addon/pihole-exporter.sh
+++ tools/addon/pihole-exporter.sh
@@ -107,8 +107,8 @@ cat <<EOF >"$CONFIG_PATH"
 # https://github.com/eko/pihole-exporter/?tab=readme-ov-file#available-cli-options
 PIHOLE_PASSWORD="${pihole_PASSWORD}"
 PIHOLE_HOSTNAME="${pihole_HOSTNAME}"
-PIHOLE_PORT=${pihole_PORT:-443}"
-SKIP_TLS_VERIFICATION="${pihole_SKIP_TLS}"
+PIHOLE_PORT="${pihole_PORT:-443}"
+SKIP_TLS_VERIFICATION="${pihole_SKIP_TLS:-false}"
 PIHOLE_PROTOCOL="${pihole_SKIP_TLS:-https}"
 EOF
 msg_ok "Created configuration"
