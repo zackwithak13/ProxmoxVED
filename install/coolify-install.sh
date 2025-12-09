e59861d2edc8ad575a7b5fed0f05cf003017eaf8
CanbiZ <47820557+MickLesk@users.noreply.github.com>
2025-12-09 09:06:02 +0100

    Add Coolify

---
+++ install/coolify-install.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Copyright (c) 2021-2025 community-scripts ORG
+# Author: MickLesk (CanbiZ)
+# License: MIT | https://github.com/community-scripts/ProxmoxVED/raw/main/LICENSE
+# Source: https://coolify.io/
+
+source /dev/stdin <<<"$FUNCTIONS_FILE_PATH"
+color
+verb_ip6
+catch_errors
+setting_up_container
+network_check
+update_os
+
+msg_info "Installing Dependencies"
+$STD apt install -y \
+  git \
+  openssl
+msg_ok "Installed Dependencies"
+
+msg_warn "WARNING: This script will run an external installer from a third-party source (https://coolify.io/)."
+msg_warn "The following code is NOT maintained or audited by our repository."
+msg_warn "If you have any doubts or concerns, please review the installer code before proceeding:"
+msg_custom "${TAB3}${GATEWAY}${BGN}${CL}" "\e[1;34m" "→  https://cdn.coollabs.io/coolify/install.sh"
+echo
+read -r -p "${TAB3}Do you want to continue? [y/N]: " CONFIRM
+if [[ ! "$CONFIRM" =~ ^([yY][eE][sS]|[yY])$ ]]; then
+  msg_error "Aborted by user. No changes have been made."
+  exit 10
+fi
+
+msg_info "Installing Coolify (Patience - this installs Docker and pulls containers)"
+$STD bash <(curl -fsSL https://cdn.coollabs.io/coolify/install.sh)
+msg_ok "Installed Coolify"
+
+motd_ssh
+customize
+cleanup_lxc
