b509440a0c6c017907934d0e89d901a68782951d
Tobias <git@tobiasmeier.dev>
2025-12-13 21:49:50 +0100

    fix

--- ct/homarr.sh
+++ ct/homarr.sh
@@ -29,7 +29,7 @@ function update_script() {
     exit
   fi
 
-  if check_for_gh_release "homarr" "Homarr/homarr"; then
+  if check_for_gh_release "Homarr" "Homarr/homarr"; then
     msg_info "Stopping Services (Patience)"
     systemctl stop homarr
     systemctl stop redis-server
