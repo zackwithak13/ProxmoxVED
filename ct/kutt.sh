01d2df1834d55b8e08b36149770e96d8ba596a72
Tom Frenzel <admin@tom-frenzel.de>
2025-12-14 16:02:37 +0100

    feat: add kutt

---
+++ ct/kutt.sh
@@ -0,0 +1,65 @@
+#!/usr/bin/env bash
+source <(curl -s https://raw.githubusercontent.com/tomfrenzel/ProxmoxVED/main/misc/build.func)
+# Copyright (c) 2021-2025 community-scripts ORG
+# Author: tomfrenzel
+# License: MIT | https://github.com/tomfrenzel/ProxmoxVED/raw/main/LICENSE
+# Source: https://github.com/thedevs-network/kutt
+
+APP="Kutt"
+var_tags="${var_tags:-sharing}"
+var_cpu="${var_cpu:-1}"
+var_ram="${var_ram:-1024}"
+var_disk="${var_disk:-2}"
+var_os="${var_os:-debian}"
+var_version="${var_version:-13}"
+var_unprivileged="${var_unprivileged:-1}"
+
+header_info "$APP"
+variables
+color
+catch_errors
+
+function update_script() {
+    header_info
+    check_container_storage
+    check_container_resources
+
+    if [[ ! -d $APP_DIR ]]; then
+        msg_error "No ${APP} Installation Found!"
+        exit
+    fi
+
+    if check_for_gh_release "kutt" "thedevs-network/kutt"; then
+        msg_info "Stopping services"
+        systemctl stop kutt
+        msg_ok "Stopped services"
+
+        msg_info "Updating packages"
+        $STD apt-get update
+        $STD apt-get dist-upgrade
+        msg_ok "Updated packages"
+
+        fetch_and_deploy_gh_release "kutt" "thedevs-network/kutt" "tarball" "latest"
+
+        msg_info "Configuring Kutt"
+        cd /opt/kutt
+        npm install
+        npm run migrate
+        msg_ok "Configured Kutt"
+
+        msg_info "Starting services"
+        systemctl start kutt
+        msg_ok "Started services"
+        msg_ok "Updated successfully"
+    fi
+    exit
+}
+
+start
+build_container
+description
+
+msg_ok "Completed Successfully!\n"
+echo -e "${CREATING}${GN}${APP} setup has been successfully initialized!${CL}"
+echo -e "${INFO}${YW} Access it using the following URL:${CL}"
+echo -e "${TAB}${GATEWAY}${BGN}http://${IP}:3000${CL}"
---
+++ install/kutt-install.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/env bash
+
+# Copyright (c) 2021-2025 community-scripts ORG
+# Author: tomfrenzel
+# License: MIT | https://github.com/tomfrenzel/ProxmoxVED/raw/main/LICENSE
+# Source: https://github.com/thedevs-network/kutt
+
+source /dev/stdin <<<"$FUNCTIONS_FILE_PATH"
+color
+verb_ip6
+catch_errors
+setting_up_container
+network_check
+update_os
+
+read -r -p "${TAB3}Enter the hostname of your Kutt instance (eg kutt.domain.tld): " kutt_host
+if [[ "$kutt_host" ]]; then
+  KUTT_HOST="$kutt_host"
+fi
+
+NODE_VERSION="22" setup_nodejs
+
+fetch_and_deploy_gh_release "kutt" "thedevs-network/kutt" "tarball"
+
+msg_info "Configuring Kutt"
+cd /opt/kutt
+cp .example.env ".env"
+sed -i "s|DEFAULT_DOMAIN=localhost:3000|DEFAULT_DOMAIN=${KUTT_HOST}|g" ".env"
+sed -i "s|JWT_SECRET=|JWT_SECRET=$(openssl rand -base64 32)|g" ".env"
+npm install
+npm run migrate
+
+cat <<EOF >/etc/systemd/system/kutt.service
+[Unit]
+Description=Kutt server
+After=network-online.target
+
+[Service]
+Type=simple
+WorkingDirectory=/opt/kutt
+ExecStart=/usr/bin/node server/server.js  --production
+Restart=always
+
+[Install]
+WantedBy=multi-user.target
+EOF
+systemctl enable -q --now kutt.service
+msg_ok "Configured Kutt"
+
+motd_ssh
+customize
+
+# Cleanup
+msg_info "Cleaning up"
+$STD apt-get -y autoremove
+$STD apt-get -y autoclean
+msg_ok "Cleaned"
