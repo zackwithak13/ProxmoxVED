c84485d5f4f221f524d2291ea7cd3c0c64150acb
CanbiZ <47820557+MickLesk@users.noreply.github.com>
2025-12-17 10:06:27 +0100

    Update debian-install.sh

--- install/debian-install.sh
+++ install/debian-install.sh
@@ -13,6 +13,8 @@ setting_up_container
 network_check
 update_os
 
+setup_hwaccel
+
 msg_info "Installing Base Dependencies"
 $STD apt-get install -y curl wget ca-certificates
 msg_ok "Installed Base Dependencies"
