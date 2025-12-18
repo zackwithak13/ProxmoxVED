a525cada4c5b4b002c18db8488979b1cd7f1087f
MintHCM-admin <admin@minthcm.org>
2025-12-18 11:15:34 +0100

    revert urls change

--- ct/minthcm.sh
+++ ct/minthcm.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-source <(curl -fsSL https://raw.githubusercontent.com/minthcm/ProxmoxVED/feature/minthcm/misc/build.func)
+source <(curl -fsSL https://raw.githubusercontent.com/community-scripts/ProxmoxVED/main/misc/build.func)
 # Copyright (c) 2021-2025 minthcm
 # Author: MintHCM
 # License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
