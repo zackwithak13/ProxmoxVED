584c09fb6726c3530953e5329a4528e8641ab5f8
GoldenSpring <GoldenSpringness@proton.me>
2025-12-23 00:08:43 +0300

    changed rejects from pr review

--- ct/rustypaste.sh
+++ ct/rustypaste.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-source <(curl -s raw.githubusercontent.com/GoldenSpringness/ProxmoxVED/refs/heads/feature/rustypaste/misc/build.func)
+source <(curl -s https://raw.githubusercontent.com/GoldenSpringness/ProxmoxVED/main/misc/misc/build.func)
 # Copyright (c) 2021-2025 community-scripts ORG
 # Author: GoldenSpringness
 # License: MIT | https://github.com/GoldenSpringness/ProxmoxVED/raw/main/LICENSE
@@ -40,9 +40,11 @@ function update_script() {
         msg_ok "Backup Created"
 
         msg_info "Updating rustypaste to latest"
+
         cd /opt/rustypaste
 
         CLEAN_INSTALL=1 fetch_and_deploy_gh_release "rustypaste" "orhun/rustypaste" "tarball" "latest" "/opt/rustypaste"
+
 
         cargo build --locked --release
 
