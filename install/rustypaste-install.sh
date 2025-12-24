35bb3bbbfc2ff06127a62e9004d2353ea8d7ceeb
Tobias <96661824+CrazyWolf13@users.noreply.github.com>
2025-12-24 15:33:34 +0100

    fix url

--- install/rustypaste-install.sh
+++ install/rustypaste-install.sh
@@ -2,7 +2,7 @@
 
 # Copyright (c) 2021-2025 community-scripts ORG
 # Author: GoldenSpringness
-# License: MIT | https://github.com/GoldenSpringness/ProxmoxVED/raw/main/LICENSE
+# License: MIT | https://github.com/community-scripts/ProxmoxVED/raw/main/LICENSE
 # Source: https://github.com/orhun/rustypaste
 
 source /dev/stdin <<<"$FUNCTIONS_FILE_PATH"
