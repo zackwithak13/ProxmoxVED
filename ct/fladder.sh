400dfeb8a0f083296adeae81bc954c0f3285fb6c
Wendy Liga <16457495+wendyliga@users.noreply.github.com>
2025-12-27 00:31:43 +0700

    fix: update license URL in fladder scripts

--- ct/fladder.sh
+++ ct/fladder.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-source <(curl -fsSL https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/misc/build.func)
+source <(curl -fsSL https://raw.githubusercontent.com/community-scripts/ProxmoxVED/main/misc/build.func)
 # Copyright (c) 2021-2025 community-scripts ORG
 # Author: wendyliga
-# License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
+# License: MIT | https://github.com/community-scripts/ProxmoxVED/raw/main/LICENSE
 # Source: https://github.com/DonutWare/Fladder
 
 APP="Fladder"
--- install/fladder-install.sh
+++ install/fladder-install.sh
@@ -2,7 +2,7 @@
 
 # Copyright (c) 2021-2025 community-scripts ORG
 # Author: wendyliga
-# License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
+# License: MIT | https://github.com/community-scripts/ProxmoxVED/raw/main/LICENSE
 # Source: https://github.com/DonutWare/Fladder
 
 source /dev/stdin <<<"$FUNCTIONS_FILE_PATH"
