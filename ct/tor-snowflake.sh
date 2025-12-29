e22edd600a1bd821d42ed8095cdd4e767f15f822
KernelSailor <git@kernelsailor.de>
2025-12-29 11:47:51 +0100

    rm info notification at the end of script

--- ct/tor-snowflake.sh
+++ ct/tor-snowflake.sh
@@ -66,6 +66,3 @@ build_container
 description
 
 msg_ok "Completed Successfully!\n"
-echo -e "${CREATING}${GN}Tor Snowflake setup has been successfully initialized!${CL}"
-echo -e "${INFO}${YW} Snowflake proxy is running as a systemd service.${CL}"
-echo -e "${INFO}${YW} Check status: systemctl status snowflake-proxy${CL}"
