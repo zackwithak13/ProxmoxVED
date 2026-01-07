a8fcd742b5033146cb1301e65e92aebb9abbd596
tremor021 <arezina.slavisa@gmail.com>
2026-01-07 21:32:39 +0100

    add cronmaster ct

---
+++ ct/cronmaster.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+source <(curl -fsSL https://git.community-scripts.org/community-scripts/ProxmoxVED/raw/branch/main/misc/build.func)
+# Copyright (c) 2021-2026 community-scripts ORG
+# Author: MickLesk (CanbiZ)
+# License: MIT | https://github.com/community-scripts/ProxmoxVED/raw/main/LICENSE
+# Source:
+
+APP="CRONMASTER"
+var_tags="${var_tags:-}"
+var_cpu="${var_cpu:-4}"
+var_ram="${var_ram:-8192}"
+var_disk="${var_disk:-20}"
+var_os="${var_os:-debian}"
+var_version="${var_version:-13}"
+var_unprivileged="${var_unprivileged:-1}"
+var_gpu="${var_gpu:-yes}"
+#var_fuse="${var_fuse:-no}"
+#var_tun="${var_tun:-no}"
+
+header_info "$APP"
+variables
+color
+catch_errors
+
+function update_script() {
+  header_info
+  check_container_storage
+  check_container_resources
+  if [[ ! -d /opt/cronmaster ]]; then
+    msg_error "No ${APP} Installation Found!"
+    exit
+  fi
+  msg_info "Updating Debian LXC"
+  $STD apt update
+  $STD apt upgrade -y
+  msg_ok "Updated Debian LXC"
+  cleanup_lxc
+  exit
+}
+
+start
+build_container
+description
+
+msg_ok "Completed successfully!"
+msg_custom "🚀" "${GN}" "${APP} setup has been successfully initialized!"
