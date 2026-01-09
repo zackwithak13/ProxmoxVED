0b73dfe1b2271a13a4a07cb227ff9f4d7b02df3a
Marc Went <marc@went.io>
2026-01-09 10:11:03 +0100

    fix review

--- install/wishlist-install.sh
+++ install/wishlist-install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# Copyright (c) 2021-2025 tteck
+# Copyright (c) 2021-2026 community-scripts ORG
 # Author: Dunky13
 # License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
 # Source: https://github.com/cmintey/wishlist
