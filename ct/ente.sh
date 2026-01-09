c93788cf8942d8dabf59e802e3cd70b7a908f91d
MickLesk <mickey.leskowitz@gmail.com>
2026-01-09 22:42:21 +0100

    fix ente

--- ct/ente.sh
+++ ct/ente.sh
@@ -8,10 +8,10 @@ source <(curl -fsSL https://git.community-scripts.org/community-scripts/ProxmoxV
 APP="Ente"
 var_tags="${var_tags:-photos}"
 var_cpu="${var_cpu:-4}"
-var_ram="${var_ram:-4096}"
-var_disk="${var_disk:-10}"
+var_ram="${var_ram:-6144}"
+var_disk="${var_disk:-20}"
 var_os="${var_os:-debian}"
-var_version="${var_version:-12}"
+var_version="${var_version:-13}"
 var_unprivileged="${var_unprivileged:-1}"
 
 header_info "$APP"
@@ -20,18 +20,18 @@ color
 catch_errors
 
 function update_script() {
-    header_info
-    check_container_storage
-    check_container_resources
-    if [[ ! -d /var ]]; then
-        msg_error "No ${APP} Installation Found!"
-        exit
-    fi
-    msg_info "Updating Ente LXC"
-    $STD apt-get update
-    $STD apt-get -y upgrade
-    msg_ok "Updated Ente LXC"
+  header_info
+  check_container_storage
+  check_container_resources
+  if [[ ! -d /var ]]; then
+    msg_error "No ${APP} Installation Found!"
     exit
+  fi
+  msg_info "Updating Ente LXC"
+  $STD apt-get update
+  $STD apt-get -y upgrade
+  msg_ok "Updated Ente LXC"
+  exit
 }
 
 start
