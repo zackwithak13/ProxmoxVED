1d14e06df870f082e2b957c9d66194d33cc31b52
Luís Oliveira <48201890+luismco@users.noreply.github.com>
2026-01-14 13:58:21 +0000

    Update install/nodecast-tv-install.sh
    Co-authored-by: greptile-apps[bot] <165735046+greptile-apps[bot]@users.noreply.github.com>

--- install/nodecast-tv-install.sh
+++ install/nodecast-tv-install.sh
@@ -2,7 +2,7 @@
 
 # Copyright (c) 2021-2026 community-scripts ORG
 # Author: luismco
-# License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
+# License: MIT | https://github.com/community-scripts/ProxmoxVED/raw/main/LICENSE
 # Source: https://github.com/technomancer702/nodecast-tv
 
 source /dev/stdin <<<"$FUNCTIONS_FILE_PATH"
