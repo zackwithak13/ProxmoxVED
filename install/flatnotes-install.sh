3b609a31fc2d73a879941c328938fc14afd18125
luismco <luismco12@gmail.com>
2026-01-15 10:13:12 +0000

    Ready for PR

--- install/flatnotes-install.sh
+++ install/flatnotes-install.sh
@@ -3,7 +3,7 @@
 # Copyright (c) 2021-2026 community-scripts ORG
 # Author: luismco
 # License: MIT | https://github.com/community-scripts/ProxmoxVED/raw/main/LICENSE
-# Source: https://github.com/technomancer702/nodecast-tv
+# Source: https://github.com/dullage/flatnotes
 
 source /dev/stdin <<<"$FUNCTIONS_FILE_PATH"
 color
