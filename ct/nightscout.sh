f23c0b72fcd4181d9fc8cb5fcc7ca5748fd787e8
Francesco Vattiato <fvattiato@gmail.com>
2026-01-18 06:18:02 +0100

    fix: Update repository name in URLs from ProxmoxVE to ProxmoxVED.

--- ct/nightscout.sh
+++ ct/nightscout.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-source <(curl -s https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/misc/build.func)
+source <(curl -s https://raw.githubusercontent.com/community-scripts/ProxmoxVED/main/misc/build.func)
 # Copyright (c) 2021-2026 community-scripts ORG
 # Author: aendel
-# License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
+# License: MIT | https://github.com/community-scripts/ProxmoxVED/raw/main/LICENSE
 # Source: https://github.com/nightscout/cgm-remote-monitor
 
 APP="Nightscout"
--- install/nightscout-install.sh
+++ install/nightscout-install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # Copyright (c) 2021-2026 community-scripts ORG
 # Author: aendel
-# License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
+# License: MIT | https://github.com/community-scripts/ProxmoxVED/raw/main/LICENSE
 # Source: https://github.com/nightscout/cgm-remote-monitor
 
 source /dev/stdin <<<"$FUNCTIONS_FILE_PATH"
