3a6790f7c0a179912eb6194d9d249b7d93140349
Francesco Vattiato <fvattiato@gmail.com>
2026-01-19 23:11:53 +0100

    chore: Update default OS version to 13.

--- ct/nightscout.sh
+++ ct/nightscout.sh
@@ -11,7 +11,7 @@ var_cpu="${var_cpu:-2}"
 var_ram="${var_ram:-2048}"
 var_disk="${var_disk:-10}"
 var_os="${var_os:-debian}"
-var_version="${var_version:-12}"
+var_version="${var_version:-13}"
 var_unprivileged="${var_unprivileged:-1}"
 
 header_info "$APP"
