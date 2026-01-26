f91ba71b8a3d0e456fed2e1b851a1fa4fc4a8a35
Joerg Heinemann <heinemannj66@gmail.com>
2026-01-26 15:31:48 +0100

    Initial commit for ebusd
    New script for ebusd

---
+++ ct/ebusd.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+source <(curl -fsSL https://raw.githubusercontent.com/heinemannj/ProxmoxVE/main/misc/build.func)
+# Copyright (c) 2021-2026 community-scripts ORG
+# Author: Joerg Heinemann (heinemannj)
+# License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
+# Source: https://github.com/john30/ebusd
+
+APP="ebusd"
+var_tags="${var_tags:-automation}"
+var_cpu="${var_cpu:-1}"
+var_ram="${var_ram:-512}"
+var_disk="${var_disk:-2}"
+var_os="${var_os:-debian}"
+var_version="${var_version:-13}"
+var_unprivileged="${var_unprivileged:-1}"
+
+header_info "$APP"
+variables
+color
+catch_errors
+
+function update_script() {
+  header_info
+  check_container_storage
+  check_container_resources
+  if [[ ! -f /etc/apt/sources.list.d/ebusd.sources ]]; then
+    msg_error "No ${APP} Installation Found!"
+    exit
+  fi
+  msg_info "Updating $APP LXC"
+  $STD apt update
+  $STD apt -y upgrade
+  msg_ok "Updated $APP LXC"
+  msg_ok "Updated successfully!"
+  exit
+}
+
+start
+build_container
+description
+
+msg_ok "Completed successfully!\n"
+echo -e "${CREATING}${GN}${APP} setup has been successfully initialized!${CL}"
---
+++ install/ebusd-install.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+# Copyright (c) 2021-2026 community-scripts ORG
+# Author: Joerg Heinemann (heinemannj)
+# License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
+# Source: https://github.com/john30/ebusd
+
+source /dev/stdin <<<"$FUNCTIONS_FILE_PATH"
+color
+verb_ip6
+catch_errors
+setting_up_container
+network_check
+update_os
+
+msg_info "Setting up ebusd Repository"
+setup_deb822_repo \
+  "ebusd" \
+  "https://raw.githubusercontent.com/john30/ebusd-debian/master/ebusd.gpg" \
+  "https://repo.ebusd.eu/apt/default/bookworm/" \
+  "bookworm" \
+  "main"
+$STD apt update
+msg_ok "ebusd Repository setup sucessfully"
+
+msg_info "Installing ebusd"
+$STD apt install -y ebusd
+msg_ok "Installed ebusd"
+
+msg_info "Follow below instructions to make the daemon autostart:"
+msg_info "1. Edit '/etc/default/ebusd' if necessary (especially if your device is not '/dev/ttyUSB0')"
+msg_info "2. Start the daemon with 'systemctl start ebusd'"
+msg_info "3. Check the daemon status with 'systemctl status ebusd'"
+msg_info "4. Check the log file '/var/log/ebusd.log'"
+msg_info "5. Make the daemon autostart with 'systemctl enable ebusd'"
+
+motd_ssh
+customize
+cleanup_lxc
