c1f3b262ff55b15268a79df63bcb3d13552348e8
CanbiZ (MickLesk) <47820557+MickLesk@users.noreply.github.com>
2026-01-26 14:17:12 +0100

    add gpu for nodecast

--- ct/nodecast-tv.sh
+++ ct/nodecast-tv.sh
@@ -13,6 +13,7 @@ var_disk="${var_disk:-4}"
 var_os="${var_os:-debian}"
 var_version="${var_version:-13}"
 var_unprivileged="${var_unprivileged:-1}"
+var_gpu="${var_gpu:-yes}"
 
 header_info "$APP"
 variables
