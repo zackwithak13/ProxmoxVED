dc379e05ed5f1a133cdf04b3756252826ef035c8
Tobias <96661824+CrazyWolf13@users.noreply.github.com>
2026-01-28 14:52:08 +0100

    Update vikunja-install.sh

--- install/vikunja-install.sh
+++ install/vikunja-install.sh
@@ -15,13 +15,13 @@ update_os
 
 fetch_and_deploy_gh_release "vikunja" "go-vikunja/vikunja" "binary" "latest" 
 
-msg_info "Setup Vikunja"
+msg_info "Setting up Vikunja"
 sed -i -E 's/^# cors:/cors:/; s/^[[:space:]]*# enable:[[:space:]]*true/  enable: false/' /etc/vikunja/config.yml
 sed -i 's|^  timezone: .*|  timezone: UTC|' /etc/vikunja/config.yml
 sed -i 's|"./vikunja.db"|"/etc/vikunja/vikunja.db"|' /etc/vikunja/config.yml
 sed -i 's|./files|/etc/vikunja/files|' /etc/vikunja/config.yml
 systemctl enable -q --now vikunja
-msg_ok "Setting up Vikunja"
+msg_ok "Set up Vikunja"
 
 motd_ssh
 customize
