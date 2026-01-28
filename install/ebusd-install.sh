e1be251201474ad35cef44fbf15a5dce47327d88
Joerg Heinemann <heinemannj66@gmail.com>
2026-01-28 13:16:35 +0100

    Add ebusd configuration instructions to a text file
    Added configuration instructions for ebusd in a new text file.

--- install/ebusd-install.sh
+++ install/ebusd-install.sh
@@ -25,6 +25,41 @@ $STD apt install -y ebusd
 $STD systemctl enable ebusd
 msg_ok "Installed ebusd"
 
+cat <<EOF >~/ebusd-configuation-instructions.txt
+Configuration instructions:
+
+	1. Edit "/etc/default/ebusd" if necessary (especially if your device is not "/dev/ttyUSB0")
+	2. Start the daemon with "systemctl start ebusd"
+	3. Check the log file "/var/log/ebusd.log"
+	4. Make the daemon autostart with "systemctl enable ebusd"
+
+Working "/etc/default/ebusd" options for "ebus adapter shield v5":
+
+EBUSD_OPTS="
+	--pidfile=/run/ebusd.pid
+	--latency=100
+	--scanconfig
+	--configpath=https://cfg.ebusd.eu/
+	--accesslevel=*
+	--pollinterval=30
+	--device=ens:XXX.XXX.XXX.XXX:9999
+	--mqtthost=XXX.XXX.XXX.XXX
+	--mqttport=1883
+	--mqttuser=XXXXXX
+	--mqttpass=XXXXXX
+	--mqttint=/etc/ebusd/mqtt-hassio.cfg
+	--mqttjson
+	--mqttlog
+	--mqttretain
+	--mqtttopic=ebusd
+	--log=all:notice
+	--log=main:notice
+	--log=bus:notice
+	--log=update:notice
+	--log=network:notice
+	--log=other:notice"
+EOF
+
 motd_ssh
 customize
 cleanup_lxc
