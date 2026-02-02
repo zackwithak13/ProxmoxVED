36e88183f6b61532f06a9457e81ea3c85afe281e
CanbiZ (MickLesk) <47820557+MickLesk@users.noreply.github.com>
2026-02-02 10:16:00 +0100

    Update vaultwarden-install.sh

--- install/vaultwarden-install.sh
+++ install/vaultwarden-install.sh
@@ -32,14 +32,17 @@ cd /tmp/vaultwarden-src
 $STD cargo build --features "sqlite,mysql,postgresql" --release
 msg_ok "Built Vaultwarden"
 
+msg_info "Setting up Vaultwarden"
 $STD addgroup --system vaultwarden
 $STD adduser --system --home /opt/vaultwarden --shell /usr/sbin/nologin --no-create-home --gecos 'vaultwarden' --ingroup vaultwarden --disabled-login --disabled-password vaultwarden
 mkdir -p /opt/vaultwarden/{bin,data,web-vault}
 cp target/release/vaultwarden /opt/vaultwarden/bin/
 cd ~ && rm -rf /tmp/vaultwarden-src
+msg_ok "Set up Vaultwarden"
 
 fetch_and_deploy_gh_release "vaultwarden_webvault" "dani-garcia/bw_web_builds" "prebuild" "latest" "/opt/vaultwarden/web-vault" "bw_web_*.tar.gz"
 
+msg_info "Configuring Vaultwarden"
 cat <<EOF >/opt/vaultwarden/.env
 ADMIN_TOKEN=''
 ROCKET_ADDRESS=0.0.0.0
@@ -49,17 +52,17 @@ DATABASE_MAX_CONNS=10
 WEB_VAULT_FOLDER=/opt/vaultwarden/web-vault
 WEB_VAULT_ENABLED=true
 EOF
-
 mv /etc/ssl/certs/ssl-cert-snakeoil.pem /opt/vaultwarden/
 mv /etc/ssl/private/ssl-cert-snakeoil.key /opt/vaultwarden/
 
-msg_info "Creating Service"
 chown -R vaultwarden:vaultwarden /opt/vaultwarden/
 chown root:root /opt/vaultwarden/bin/vaultwarden
 chmod +x /opt/vaultwarden/bin/vaultwarden
 chown -R root:root /opt/vaultwarden/web-vault/
 chmod +r /opt/vaultwarden/.env
+msg_ok "Configured Vaultwarden"
 
+msg_info "Creating Service"
 cat <<'EOF' >/etc/systemd/system/vaultwarden.service
 [Unit]
 Description=Bitwarden Server (Powered by Vaultwarden)
@@ -92,7 +95,7 @@ AmbientCapabilities=CAP_NET_BIND_SERVICE
 [Install]
 WantedBy=multi-user.target
 EOF
-systemctl enable --q -now vaultwarden
+systemctl enable -q --now vaultwarden
 msg_ok "Created Service"
 
 motd_ssh
